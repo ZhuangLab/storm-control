56699d85504caca555bd33b3962ead5d4526042f
Hazen Babcock <hbabcock@forge.abcd.harvard.edu>
2013-08-07 12:09:03 +0000

    Changes to tcpClient:   1. Remove testing mode.   2. Port is created once, then opened and closes rather than creating a new one each time we connect.   3. 4 attempts are made to connect before giving up. The status of the connection is recorded.   4. sendCommand does nothing if not connected.
    Changed (branch/hazen) steve to remove all the connect / disconnect GUI infrastructure, connections are made automatically if needed.

--- hal4000/halLib/tcpClient.py
+++ hal4000/halLib/tcpClient.py
@@ -18,17 +18,28 @@ class HALSocket(QtNetwork.QTcpSocket):
 
     def __init__(self, port):
         QtNetwork.QTcpSocket.__init__(self)
+        self.connected = False
+        self.port = port
 
         # signals
-        self.connect(self, QtCore.SIGNAL("readyRead()"), self.handleReadyRead)
+        self.readyRead.connect(self.handleReadyRead)
 
+    def connectToHAL(self):
         # try to make connection
         addr = QtNetwork.QHostAddress(QtNetwork.QHostAddress.LocalHost)
-        self.connectToHost(addr, port)
-        while not self.waitForConnected():
+        self.connectToHost(addr, self.port)
+        tries = 0
+        while (not self.waitForConnected() and (tries < 5)):
             print "Couldn't connect to HAL-4000, waiting 1 second and retrying"
             time.sleep(1)
+            tries += 1
             self.connectToHost(addr, port)
+
+        if (tries == 5):
+            print "Could not connect to HAL-4000."
+            self.connected = False
+        else:
+            self.connected = True
 
     def handleReadyRead(self):
         while self.canReadLine():
@@ -53,17 +64,17 @@ class TCPClient(QtGui.QWidget):
 
     def __init__(self, parent = None):
         QtGui.QWidget.__init__(self, parent)
-        self.connected = 0
+        self.connected = False
+        self.socket = HALSocket(9000)
         self.state = None
-        self.testing = 0
+        self.testing = False
         self.unacknowledged = 0
 
-        # For testing without actually talking to HAL-4000
-        if self.testing:
-            self.testing_timer = QtCore.QTimer(self)
-            self.testing_timer.setInterval(5000)
-            self.testing_timer.setSingleShot(True)
-            self.testing_timer.timeout.connect(self.handleComplete)
+        self.socket.acknowledged.connect(self.handleAcknowledged)
+        self.socket.complete.connect(self.handleComplete)
+
+    def getConnected(self):
+        return self.connected
 
     def handleAcknowledged(self):
         print " got response"
@@ -94,26 +105,23 @@ class TCPClient(QtGui.QWidget):
         else:
             print "Not connected?!?"
 
-    def sendInitialPowers(self, movie):
-        if self.testing:
-            print "sending initial powers"
+    #def sendInitialPowers(self, movie):
+    #    if self.testing:
+    #        print "sending initial powers"
 
-    # Fire all the data off at once...
+    # Fire all the settings off at once...
     def sendMovieParameters(self, movie):
-        if self.testing:
-            print "sending movie parameters"
-        else:
-            if self.unacknowledged != 0:
-                print self.unacknowledged, "commands are still pending"
-            # send parameters index.
-            if hasattr(movie, "parameters"):
-                self.sendCommand("parameters,int,{0:d}".format(movie.parameters))
-            # send stage position.
-            if hasattr(movie, "stage_x") and hasattr(movie, "stage_y"):
-                self.sendCommand("moveTo,float,{0:.2f},float,{1:.2f}".format(movie.stage_x, movie.stage_y))
-            # send lock target.
-            if hasattr(movie, "lock_target"):
-                self.sendCommand("setLockTarget,float,{0:.1f}".format(movie.lock_target))
+        if self.unacknowledged != 0:
+            print self.unacknowledged, "commands are still pending"
+        # send parameters index.
+        if hasattr(movie, "parameters"):
+            self.sendCommand("parameters,int,{0:d}".format(movie.parameters))
+        # send stage position.
+        if hasattr(movie, "stage_x") and hasattr(movie, "stage_y"):
+            self.sendCommand("moveTo,float,{0:.2f},float,{1:.2f}".format(movie.stage_x, movie.stage_y))
+        # send lock target.
+        if hasattr(movie, "lock_target"):
+            self.sendCommand("setLockTarget,float,{0:.1f}".format(movie.lock_target))
 
     #def sendPowerUpdate(self, channel, power_increment):
     #    if self.testing:
@@ -122,18 +130,14 @@ class TCPClient(QtGui.QWidget):
     #        self.sendCommand("incPower,int,{0:d},float,{1:.4f}".format(channel, power_increment))
 
     def sendSetDirectory(self, directory):
-        if self.testing:
-            print " send set directory", directory
-        else:
-            self.sendCommand("setDirectory,string,{0:s}".format(directory))
+        self.sendCommand("setDirectory,string,{0:s}".format(directory))
 
     def startCommunication(self):
         print "starting communications"
-        self.socket = HALSocket(9000)
-        self.connected = 1
-        self.unacknowledged = 0
-        self.socket.acknowledged.connect(self.handleAcknowledged)
-        self.socket.complete.connect(self.handleComplete)
+        if not self.connected:
+            self.socket.connectToHAL()
+            self.connected = self.socket.connected
+            self.unacknowledged = 0
 
     def startFindSum(self):
         print "start find sum"
@@ -142,30 +146,27 @@ class TCPClient(QtGui.QWidget):
 
     def startMovie(self, movie):
         print "start movie"
-        if self.testing:
-            self.testing_timer.start()
-        else:
-            if hasattr(movie, "progression"):
-                if (movie.progression.type == "lockedout"):
-                    self.sendCommand("progressionLockout")
-                elif (movie.progression.type != "none"):
-                    self.sendCommand("progressionType,string,{0:s}".format(movie.progression.type))
-                    # Power controlled by file.
-                    if movie.progression.type == "file":
-                        self.sendCommand("progressionFile,string,{0:s}".format(movie.progression.filename))
-                    else:
-                        for channel in movie.progression.channels:
-                            # Power controlled by progression dialog box settings.
-                            if channel[2]:
-                                self.sendCommand("progressionSet,int,{0:d},float,{1:.4f},int,{2:d},float,{3:.4f}".format(channel[0],
-                                                                                                                         channel[1],
-                                                                                                                         channel[2],
-                                                                                                                         channel[3]))
-                            # Fixed power.
-                            else:
-                                self.sendCommand("setPower,int,{0:d},float,{1:.4f}".format(channel[0], channel[1]))
-            self.state = "filming"
-            self.sendCommand("movie,string,{0:s},int,{1:d}".format(movie.name, movie.length))
+        if hasattr(movie, "progression"):
+            if (movie.progression.type == "lockedout"):
+                self.sendCommand("progressionLockout")
+            elif (movie.progression.type != "none"):
+                self.sendCommand("progressionType,string,{0:s}".format(movie.progression.type))
+                # Power controlled by file.
+                if movie.progression.type == "file":
+                    self.sendCommand("progressionFile,string,{0:s}".format(movie.progression.filename))
+                else:
+                    for channel in movie.progression.channels:
+                        # Power controlled by progression dialog box settings.
+                        if channel[2]:
+                            self.sendCommand("progressionSet,int,{0:d},float,{1:.4f},int,{2:d},float,{3:.4f}".format(channel[0],
+                                                                                                                     channel[1],
+                                                                                                                     channel[2],
+                                                                                                                     channel[3]))
+                        # Fixed power.
+                        else:
+                            self.sendCommand("setPower,int,{0:d},float,{1:.4f}".format(channel[0], channel[1]))
+        self.state = "filming"
+        self.sendCommand("movie,string,{0:s},int,{1:d}".format(movie.name, movie.length))
 
     def startRecenterPiezo(self):
         print "start recenter piezo"
@@ -174,19 +175,13 @@ class TCPClient(QtGui.QWidget):
 
     def stopCommunication(self):
         print "stopping communications"
-        if self.unacknowledged == 0:
+        if self.connected:
             self.socket.disconnectFromHost()
-            return True
-        else:
-            return False
+            self.connected = False
 
     def stopMovie(self):
         print "stop movie"
-        if self.testing:
-            self.testing_timer.stop()
-            self.complete.emit()
-        else:
-            self.sendCommand("abortMovie")
+        self.sendCommand("abortMovie")
 
 #
 # The MIT License
