850ea1d7715ccb8101b76f0af829a2c58aa26097
Hazen Babcock <hbabcock@forge.abcd.harvard.edu>
2013-11-16 00:32:38 +0000

    More work on getting logging to work in a useful way. This remains to be tested on a real setup, but seems to work for the none setup.

--- hal4000/halLib/hdebug.py
+++ hal4000/halLib/hdebug.py
@@ -11,78 +11,44 @@
 
 import logging
 import logging.handlers
-import sys
-import time
 
 from PyQt4 import QtCore
 
-want_debugging = False
+a_logger = False
+logging_mutex = QtCore.QMutex()
 
 ## debug
 #
-# Function decorator. This prints all the arguments to a function that it decorates.
+# Function decorator. This logs all the arguments to a function that it decorates
+# if logging has been started.
 #
 def debug(fn):
-    def debug_f(*args, **kw):
-        if fn.__module__ == "__main__":
-            print fn.__module__ + "." + fn.__name__ + ": " + str(time.time())
-            for i, arg in enumerate(args):
-                print "    " + str(i) + ".", arg
-        else:
-            print "  " + fn.__module__ + "." + fn.__name__ + ": " + str(time.time())
-            for i, arg in enumerate(args):
-                print "      " + str(i) + ".", arg
+    global a_logger, logging_mutex
+    def wrap(*args, **kw):
+        if a_logger:
+            logging_mutex.lock()
+            if fn.__module__ == "__main__":
+                a_logger.info(fn.__module__ + "." + fn.__name__)
+                for i, arg in enumerate(args):
+                    a_logger.info("    " + str(i) + " " + str(arg))
+            else:
+                a_logger.info("  " + fn.__module__ + "." + fn.__name__)
+                for i, arg in enumerate(args):
+                    a_logger.info("      " + str(i) + " " + str(arg))
+            logging_mutex.unlock()
         return fn(*args, **kw)
-    global want_debugging
-    if want_debugging:
-        return debug_f
-    else:
-        return fn
-
-## debugSlot
-#
-# Slot decorator. This prints all the signal arguments to a slot.
-#
-def debugSlot(fn):
-    def debug_f(*args):
-        if fn.__module__ == "__main__":
-            print fn.__module__ + "." + fn.__name__ + ": " + str(time.time())
-            for i, arg in enumerate(args):
-                print "    " + str(i) + ".", arg
-        else:
-            print "  " + fn.__module__ + "." + fn.__name__ + ": " + str(time.time())
-            for i, arg in enumerate(args):
-                print "      " + str(i) + ".", arg
-        return fn(*args[:-1])
-    global want_debugging
-    if want_debugging:
-        return debug_f
-    else:
-        return fn
+    return wrap
 
 ## getDebug
 #
-# @return True/False is debugging information desired.
+# @return True/False if debugging information desired.
 #
 def getDebug():
-    return want_debugging
-
-## setDebugging
-#
-# Sets the debugging flag.
-# 
-# FIXME: This doesn't actually work as you
-# need to set the debugging flag at compile time, run time is
-# too late as all the decorators have already been created.
-#
-# @param state True/False the desired debugging state.
-#
-def setDebugging(state):
-    global want_debugging
-    if state:
-        want_debugging = True
+    global a_logger
+    if a_logger:
+        return True
     else:
-        want_debugging = False
+        return False
 
 ## startLogging
 #
@@ -92,66 +58,21 @@ def setDebugging(state):
 # @param program_name The name of the program that is doing the logging.
 #
 def startLogging(directory, program_name):
+    global a_logger
 
     # Initialize logger.
     a_logger = logging.getLogger(program_name)
     a_logger.setLevel(logging.DEBUG)
 
-    # Create formatters.
+    # Create formatter.
     rt_formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
-    st_formatter = logging.Formatter('%(message)s')
 
     # Rotating file handle for saving output.
     rf_handler = logging.handlers.RotatingFileHandler(directory + program_name + ".out",
-                                                      maxBytes = 10000,
+                                                      maxBytes = 20000,
                                                       backupCount = 5)
     rf_handler.setFormatter(rt_formatter)
     a_logger.addHandler(rf_handler)
-
-    # Stream handler for console output.
-    st_handler = logging.StreamHandler()
-    st_handler.setFormatter(st_formatter)
-    a_logger.addHandler(st_handler)
-
-    # Set to capture stdout.
-    sys.stdout = StreamToLogger(a_logger, logging.INFO)
-
-    # Set to capture stderr.
-    sys.stderr = StreamToLogger(a_logger, logging.ERROR)
-
-## StreamToLogger
-#
-# This is basically a copy of the code from here:
-#  http://www.electricmonk.nl/log/2011/08/14/redirect-stdout-and-stderr-to-a-logger-in-python/
-#    
-# FIXME: The print command is thread safe, but this is not, so capturing print
-#   statements from a program with multiple threads like HAL will cause it to freeze.
-#
-class StreamToLogger(object):
-
-    ## __init__
-    #
-    # Create the stream logging object.
-    #
-    # @param logger A logging object.
-    # @param log_level What level logging to use (info, error, etc.).
-    #
-    def __init__(self, logger, log_level):
-        self.logger = logger
-        self.log_level = log_level
-        self.write_mutex = QtCore.QMutex()
-
-    ## write
-    #
-    # Called when new text is sent to the stream.
-    #
-    # @param buf The text string.
-    #
-    def write(self, buf):
-        self.write_mutex.lock()
-        for line in buf.rstrip().splitlines():
-            self.logger.log(self.log_level, line.rstrip())
-        self.write_mutex.unlock()
 
 #
 # The MIT License
