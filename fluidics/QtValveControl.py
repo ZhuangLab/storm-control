c125f2f67975ce585a691dc4a4647c11fbd4528f
Jeffrey Moffitt <jeffmoffitt@gmail.com>
2013-12-22 10:11:35 -0500

    Removed .valve_widget to simplify QtValveControl and to allow direct access to methods of this class

--- fluidics/QtValveControl.py
+++ fluidics/QtValveControl.py
@@ -25,13 +25,13 @@ class QtValveControl(QtValveControlWidget):
         QtValveControlWidget.__init__(self, parent)
             
         # create widget
-        self.valve_widget = QtValveControlWidget()
+        #self.valve_widget = QtValveControlWidget()
         
         # Set internal variables
         self.valve_ID = ID
         self.max_ports = len(port_names)
         self.max_rotation = len(rotation_directions)
-        self.valve_widget.verbose = verbose
+        self.verbose = verbose
         
         # Configure display objects
         self.setValveName(valve_name)
@@ -44,61 +44,61 @@ class QtValveControl(QtValveControlWidget):
         self.setError(error)
 
         # Connect signal to change port button
-        self.valve_widget.changePortButton.clicked.connect(self.changePortSignal)
+        self.changePortButton.clicked.connect(self.changePortSignal)
 
     def setValveName(self, name):
-        self.valve_widget.valveGroupBox.setTitle(name)
+        self.valveGroupBox.setTitle(name)
 
     def getValveName(self):
-        return self.valve_widget.valveGroupBox.title()
+        return self.valveGroupBox.title()
 
     def setValveConfiguration(self, configuration):
-        self.valve_widget.valveConfigurationLabel.setText(configuration)
+        self.valveConfigurationLabel.setText(configuration)
 
     def getValveConfiguration(self):
-        return self.valve_widget.valveConfigurationLabel.text()
+        return self.valveConfigurationLabel.text()
     
     def setPortNames(self, port_names):
-        self.valve_widget.desiredPortComboBox.clear()
+        self.desiredPortComboBox.clear()
 
         for name in port_names:
-            self.valve_widget.desiredPortComboBox.addItem(name)
+            self.desiredPortComboBox.addItem(name)
 
     def setCurrentDesiredPort(self, desired_port):
         if (desired_port > (self.max_ports -1 )):
             desired_port = 0
-        self.valve_widget.desiredPortComboBox.setCurrentIndex(desired_port)
+        self.desiredPortComboBox.setCurrentIndex(desired_port)
 
     def getPortIndex(self):
-        return self.valve_widget.desiredPortComboBox.currentIndex()
+        return self.desiredPortComboBox.currentIndex()
 
     def setRotationDirections(self, rotation_directions):
-        self.valve_widget.desiredRotationComboBox.clear()
+        self.desiredRotationComboBox.clear()
         for name in rotation_directions:
-            self.valve_widget.desiredRotationComboBox.addItem(name)
+            self.desiredRotationComboBox.addItem(name)
 
     def setCurrentDesiredRotation(self, desired_rotation):
         if (desired_rotation > (self.max_rotation -1 )):
             desired_rotation = 0
-        self.valve_widget.desiredRotationComboBox.setCurrentIndex(desired_rotation) 
+        self.desiredRotationComboBox.setCurrentIndex(desired_rotation) 
 
     def getDesiredRotationIndex(self):
-        return self.valve_widget.desiredRotationComboBox.currentIndex()
+        return self.desiredRotationComboBox.currentIndex()
 
     def setStatus(self, status):
         # Set Label Text
-        self.valve_widget.valveStatusLabel.setText(status[0])
+        self.valveStatusLabel.setText(status[0])
 
         if status[1] == True:
-            self.valve_widget.valveStatusLabel.setStyleSheet("QLabel { color: red}")
+            self.valveStatusLabel.setStyleSheet("QLabel { color: red}")
         if status[1] == False:
-            self.valve_widget.valveStatusLabel.setStyleSheet("QLabel { color: black}")                
+            self.valveStatusLabel.setStyleSheet("QLabel { color: black}")                
 
     def setError(self, error):
         pass
 
     def changePortSignal(self):
-        if self.valve_widget.verbose:
+        if self.verbose:
             print ("Emitting signal from " + self.getValveName() +
                    ": Desired port index " + str(self.getPortIndex()) +
                    " and direction index " + str(self.getDesiredRotationIndex())
@@ -132,7 +132,7 @@ class Window(QtGui.QMainWindow):
         for ID in range(3):
             wid = QtValveControl(self, ID = ID, valve_name = "Valve " + str(ID))
             wid.change_port_signal.connect(self.detectEmittedSignal)
-            self.valve_widgets.append(wid.valve_widget)
+            self.valve_widgets.append(wid)
             self.scrollLayout.addWidget(self.valve_widgets[-1])
 
         self.scrollLayout.addStretch(1)
@@ -146,7 +146,7 @@ class Window(QtGui.QMainWindow):
 
     def detectEmittedSignal(self, valve_ID):
         print "Detected signal from valve index: " + str(valve_ID)
-        #print "Found port index: " + str(self.valve_widgets[valve_ID].getPortIndex())
+        print "Found port index: " + str(self.valve_widgets[valve_ID].getPortIndex())
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
