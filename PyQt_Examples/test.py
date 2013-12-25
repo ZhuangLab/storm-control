b15dc8be55f4de1f04e543c7f0c46c7b8e334403
Jeffrey Moffitt <jeffmoffitt@gmail.com>
2013-12-24 21:49:42 -0500

    Quick PyQt example of a widget wrapper around other widgets: a possible model for organizing sub GUIs?

---
+++ PyQt_Examples/test.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'MultiComponentWidget.ui'
+#
+# Created: Tue Dec 24 21:42:39 2013
+#      by: PyQt4 UI code generator 4.10.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.resize(400, 300)
+        self.widget = QtGui.QWidget(Form)
+        self.widget.setGeometry(QtCore.QRect(50, 40, 113, 132))
+        self.widget.setObjectName(_fromUtf8("widget"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.widget)
+        self.verticalLayout.setMargin(0)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.groupBox = QtGui.QGroupBox(self.widget)
+        self.groupBox.setObjectName(_fromUtf8("groupBox"))
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.groupBox)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.pushButton = QtGui.QPushButton(self.groupBox)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.verticalLayout_2.addWidget(self.pushButton)
+        self.pushButton_2 = QtGui.QPushButton(self.groupBox)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.verticalLayout_2.addWidget(self.pushButton_2)
+        self.pushButton_3 = QtGui.QPushButton(self.groupBox)
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        self.verticalLayout_2.addWidget(self.pushButton_3)
+        self.verticalLayout.addWidget(self.groupBox)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(_translate("Form", "Form", None))
+        self.groupBox.setTitle(_translate("Form", "GroupBox", None))
+        self.pushButton.setText(_translate("Form", "PushButton", None))
+        self.pushButton_2.setText(_translate("Form", "PushButton", None))
+        self.pushButton_3.setText(_translate("Form", "PushButton", None))
+
