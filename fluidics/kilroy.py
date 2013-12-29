97973e271619aef32e40df834c70cb877ea2d408
Jeffrey Moffitt <jeffmoffitt@gmail.com>
2013-12-29 12:40:36 -0500

    kilroy: Cleaned up Kilroy class.
    Added annotations and comments to kilroy in preparation for an initial public commit.

--- fluidics/kilroy.py
+++ fluidics/kilroy.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python
+# ----------------------------------------------------------------------------------------
+# A master control class to implemented a series of automated flow protocols
+# using a daisy chained valve system (and eventually syringe pumps)
+# ----------------------------------------------------------------------------------------
+# Jeff Moffitt
+# 12/28/13
+# jeffmoffitt@gmail.com
+# ----------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------
+# Import
+# ----------------------------------------------------------------------------------------
 import sys
 import os
 import time
@@ -6,19 +18,22 @@ from PyQt4 import QtCore, QtGui
 from valveChain import ValveChain
 from valveProtocols import ValveProtocols
 
+# ----------------------------------------------------------------------------------------
+# Kilroy Class Definition
+# ----------------------------------------------------------------------------------------
 class Kilroy(QtGui.QMainWindow):
-    
     def __init__(self, verbose = False):
         super(Kilroy, self).__init__()
 
         # Initialize internal attributes
         self.verbose = verbose
 
-        # Create Valve Chain
+        # Create ValveChain instance
         self.valveChain = ValveChain(COM_port = 2,
-                                         verbose = self.verbose,
-                                         num_simulated_valves = 2)
+                                     num_simulated_valves = 2,
+                                     verbose = self.verbose)
 
+        # Create ValveChain instance
         self.valveProtocols = ValveProtocols(verbose = self.verbose)
 
         # Connect command ready signal
@@ -27,16 +42,25 @@ class Kilroy(QtGui.QMainWindow):
         # Create GUI
         self.createGUI()
 
+    # ----------------------------------------------------------------------------------------
+    # Create master GUI
+    # ----------------------------------------------------------------------------------------
     def createGUI(self):
         self.mainLayout = QtGui.QGridLayout()
         self.mainLayout.addWidget(self.valveProtocols.mainWidget, 0, 0, 1, 3)
         self.mainLayout.addWidget(self.valveProtocols.valveCommands.mainWidget, 2, 0, 1, 3) 
         self.mainLayout.addWidget(self.valveChain.mainWidget, 0, 4, 1, 1)
 
+    # ----------------------------------------------------------------------------------------
+    # Redirect commands from valve protocol class to valve chain class
+    # ----------------------------------------------------------------------------------------
     def sendCommand(self):
         command = self.valveProtocols.getCurrentCommand()
         self.valveChain.receiveCommand(command)
-    
+
+# ----------------------------------------------------------------------------------------
+# Stand Alone Kilroy Class
+# ----------------------------------------------------------------------------------------                                                                   
 class StandAlone(QtGui.QMainWindow):
     def __init__(self, parent = None):
         super(StandAlone, self).__init__(parent)
@@ -57,38 +81,26 @@ class StandAlone(QtGui.QMainWindow):
         # set window geometry
         self.setGeometry(50, 50, 500, 400)
 
-        # Create file menu
-##        menubar = self.menuBar()
-##        file_menu = menubar.addMenu("File")
-##
-##        exit_action = QtGui.QAction("Exit", self)
-##        exit_action.setShortcut("Ctrl+Q")
-##        exit_action.triggered.connect(self.closeEvent)
-##
-##        file_menu.addAction(exit_action)
-##        file_menu.addAction(self.valve_chain_commands.load_commands_action)
-
-##    def closeEvent(self, event):
-##        self.valve_chain_commands.close()
-##        self.close()
-        
+# ----------------------------------------------------------------------------------------
+# Runtime code: Kilroy is meant to be run as a stand alone
+# ----------------------------------------------------------------------------------------                                
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
 
-     # Splash Screen.
-
+    # Show splash screen (to allow for valve initialization)
     splash_pix = QtGui.QPixmap("kilroy_splash.jpg")
     splash = QtGui.QSplashScreen(splash_pix, QtCore.Qt.WindowStaysOnTopHint)
     splash.setMask(splash_pix.mask())
     splash.show()
     app.processEvents()
-    
-    time.sleep(2)
-    
-    app.setWindowIcon(QtGui.QIcon('kilroy.jpg'))
-    window = StandAlone()
-    window.setWindowIcon(QtGui.QIcon('kilroy.jpg'))
+    time.sleep(2) # Define minimum startup time
 
+    # Create instance of StandAlone class
+    window = StandAlone()
+
+    # Remove splash screen
     splash.hide()
+
+    # Run main app
     window.show()
     sys.exit(app.exec_())
