014392f6e5c4d175876e5e224f994f3d5dc5721d
Jeffrey Moffitt <jeffmoffitt@gmail.com>
2013-12-29 07:51:01 -0500

    Minor cleanup of Python coded
    Deleted a spurious test script that had been saved outside the appropriate folder by accident.

--- TestGroupScrollArea.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'TestGroupBoxScrollArea.ui'
-#
-# Created: Sun Dec 22 18:55:42 2013
-#      by: PyQt4 UI code generator 4.10.3
-#
-# WARNING! All changes made in this file will be lost!
-
