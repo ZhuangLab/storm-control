01fe51aeddca27892f02661f9b66282b3e96027a
Hazen <hbabcock@fas.harvard.edu>
2014-01-06 13:36:41 -0500

    Change prism2 setup to the hardware XML approach.

--- hal4000/focuslock/prism2FocusLockZ.py
+++ hal4000/focuslock/prism2FocusLockZ.py
@@ -26,7 +26,7 @@ import focuslock.focusLockZ as focusLockZ
 # with Phresh QPD and MCL objective Z positioner.
 #
 class AFocusLockZ(focusLockZ.FocusLockZQPD):
-    def __init__(self, parameters, tcp_control, parent = None):
+    def __init__(self, hardware, parameters, tcp_control, parent = None):
         qpd = phreshQPD.PhreshQPDPRISM2()
         stage = mclController.MCLStage("c:/Program Files/NanoDrive/")
         lock_fn = lambda (x): -1.75 * x
