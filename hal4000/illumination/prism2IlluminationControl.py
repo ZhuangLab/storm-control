477c0341c05ca8553008771b756397cb485f3058
Hazen <hbabcock@fas.harvard.edu>
2014-02-18 09:36:01 -0500

    Update to modules.

--- hal4000/illumination/prism2IlluminationControl.py
+++ hal4000/illumination/prism2IlluminationControl.py
@@ -11,10 +11,12 @@ from PyQt4 import QtCore
 
 import coherent.cube405 as cube405
 import coherent.cube445 as cube445
+import nationalInstruments.nicontrol as nicontrol
 
 import illumination.channelWidgets as channelWidgets
 import illumination.commandQueues as commandQueues
 import illumination.illuminationControl as illuminationControl
+import illumination.shutterControl as shutterControl
 
 #
 # Illumination power control specialized for Prism2.
@@ -145,14 +147,96 @@ class Prism2QIlluminationControlWidget(illuminationControl.QIlluminationControlW
 
 
 #
+# Prism2 shutter control.
+#
+# Channels 0-3 are controlled by the AOTF.
+# Channel 4 is a Coherent 405 diode laser which is
+# modulated directly.
+# Channel 5 is the white light shutter.
+#
+# These are driven by the analog out lines of a
+# National Instruments PCI-6722 card.
+#
+class Prism2ShutterControl(shutterControl.ShutterControl):
+    def __init__(self, powerToVoltage, parent):
+        shutterControl.ShutterControl.__init__(self, powerToVoltage, parent)
+        self.oversampling_default = 100
+        self.number_channels = 8
+
+        self.ct_task = 0
+        self.wv_task = 0
+        self.board = "PCI-6722"
+
+        self.defaultAOTFLines()
+
+    def cleanup(self):
+        if self.ct_task:
+            self.ct_task.clearTask()
+            self.wv_task.clearTask()
+            self.ct_task = 0
+            self.wv_task = 0
+
+    def defaultAOTFLines(self):
+        for i in range(7):
+            # set analog lines to default (max).
+            nicontrol.setAnalogLine(self.board, i, self.powerToVoltage(i, 1.0))
+
+    def prepare(self):
+        # This sets things so we don't get a burst of light at the
+        # begining with all the lasers coming on.
+        for i in range(self.number_channels):
+            nicontrol.setAnalogLine(self.board, i, 0.0)
+
+    def setup(self, kinetic_cycle_time):
+        assert self.ct_task == 0, "Attempt to call setup without first calling cleanup."
+        #
+        # the counter runs slightly faster than the camera so that it is ready
+        # to catch the next camera "fire" immediately after the end of the cycle.
+        #
+        frequency = (1.001/kinetic_cycle_time) * float(self.oversampling)
+
+        # set up the analog channels
+        self.wv_task = nicontrol.WaveformOutput(self.board, 0)
+        for i in range(self.number_channels - 1):
+            self.wv_task.addChannel(i + 1)
+
+        # set up the waveform
+        self.wv_task.setWaveform(self.waveforms, frequency)
+
+        # set up the counter
+        self.ct_task = nicontrol.CounterOutput(self.board, 0, frequency, 0.5)
+        self.ct_task.setCounter(self.waveform_len)
+        self.ct_task.setTrigger(0)
+
+    def startFilm(self):
+        self.wv_task.startTask()
+        self.ct_task.startTask()
+
+    def stopFilm(self):
+        # stop the tasks
+        if self.ct_task:
+            self.ct_task.stopTask()
+            self.wv_task.stopTask()
+            self.ct_task.clearTask()
+            self.wv_task.clearTask()
+            self.ct_task = 0
+            self.wv_task = 0
+
+        # reset all the analog signals.
+        self.defaultAOTFLines()
+
+
+#
 # Illumination power control dialog box specialized for Prism2.
 #
 class AIlluminationControl(illuminationControl.IlluminationControl):
-    def __init__(self, hardware, parameters, tcp_control, parent = None):
-        illuminationControl.IlluminationControl.__init__(self, parameters, tcp_control, parent)
-        self.power_control = Prism2QIlluminationControlWidget("illumination/prism2_illumination_control_settings.xml",
+    def __init__(self, hardware, parameters, parent = None):
+        illuminationControl.IlluminationControl.__init__(self, parameters, parent)
+        self.power_control = Prism2QIlluminationControlWidget("illumination/" + hardware.settings_xml,
                                                               parameters,
                                                               parent = self.ui.laserBox)
+        self.shutter_control = Prism2ShutterControl(self.power_control.powerToVoltage,
+                                                    self.ui.laserBox)
         self.updateSize()
 
 #
