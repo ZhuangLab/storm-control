437d5735c05d896411847a95a1239ad67403664c
Jeffrey Moffitt <jeffmoffitt@gmail.com>
2014-02-26 18:07:46 -0500

    Fully Functional Test Script for Rainin RP1 Communication

--- fluidics/pumps/test_script.py
+++ fluidics/pumps/test_script.py
@@ -175,6 +175,187 @@ while not done:
 print "Received: " + str(complete_message)
 
 
+# Poll pump for ready signal
+ready = False
+ready_signal = '\n'
+not_ready_signal = chr(ord('#'))
+print "Is pump ready for buffered command?"
+while not ready:
+    serial.write('\x0A')
+    response = serial.read(1)
+    if response == ready_signal:
+        ready = True
+        print "Pump is ready to received buffered command"
+        # Clear buffer
+        response = serial.read(10)
+
+# Change Speed
+message_to_write = [chr(ord('R')), chr(ord('1')), chr(ord('0')), chr(ord('0')), chr(ord('0')), '\x0D']
+
+print "Writing: " + str(message_to_write)
+
+for message in message_to_write:
+    received = False
+    while not received:
+        print "Writing: " + message
+        serial.write(message)
+        response = serial.read(10)
+        print "Response: " + str((response, ''))
+        if response == message:
+            received = True
+        else:
+            print "Error in transmission of " + str((message, ''))
+
+# Poll pump for ready signal
+ready = False
+ready_signal = '\n'
+not_ready_signal = chr(ord('#'))
+print "Is pump ready for buffered command?"
+while not ready:
+    serial.write('\x0A')
+    response = serial.read(1)
+    if response == ready_signal:
+        ready = True
+        print "Pump is ready to received buffered command"
+        # Clear buffer
+        response = serial.read(10)
+
+
+# Start Pump
+message_to_write = [chr(ord('j')), chr(ord('F')), '\x0D']
+
+print "Writing: " + str(message_to_write)
+
+for message in message_to_write:
+    received = False
+    while not received:
+        print "Writing: " + message
+        serial.write(message)
+        response = serial.read(10)
+        print "Response: " + str((response, ''))
+        if response == message:
+            received = True
+        else:
+            print "Error in transmission of " + str((message, ''))
+
+time.sleep(5)
+
+# Poll pump for ready signal
+ready = False
+ready_signal = '\n'
+not_ready_signal = chr(ord('#'))
+print "Is pump ready for buffered command?"
+while not ready:
+    serial.write('\x0A')
+    response = serial.read(1)
+    if response == ready_signal:
+        ready = True
+        print "Pump is ready to received buffered command"
+        # Clear buffer
+        response = serial.read(10)
+
+# Change Speed
+message_to_write = [chr(ord('R')), chr(ord('3')), chr(ord('0')), chr(ord('0')), chr(ord('0')), '\x0D']
+
+print "Writing: " + str(message_to_write)
+
+for message in message_to_write:
+    received = False
+    while not received:
+        print "Writing: " + message
+        serial.write(message)
+        response = serial.read(10)
+        print "Response: " + str((response, ''))
+        if response == message:
+            received = True
+        else:
+            print "Error in transmission of " + str((message, ''))
+
+time.sleep(5)
+# Poll pump for ready signal
+ready = False
+ready_signal = '\n'
+not_ready_signal = chr(ord('#'))
+print "Is pump ready for buffered command?"
+while not ready:
+    serial.write('\x0A')
+    response = serial.read(1)
+    if response == ready_signal:
+        ready = True
+        print "Pump is ready to received buffered command"
+        # Clear buffer
+        response = serial.read(10)
+
+# Change Speed
+message_to_write = [chr(ord('R')), chr(ord('0')), chr(ord('0')), chr(ord('0')), chr(ord('0')), '\x0D']
+
+print "Writing: " + str(message_to_write)
+
+for message in message_to_write:
+    received = False
+    while not received:
+        print "Writing: " + message
+        serial.write(message)
+        response = serial.read(10)
+        print "Response: " + str((response, ''))
+        if response == message:
+            received = True
+        else:
+            print "Error in transmission of " + str((message, ''))
+
+
+# Poll pump for ready signal
+ready = False
+ready_signal = '\n'
+not_ready_signal = chr(ord('#'))
+print "Is pump ready for buffered command?"
+while not ready:
+    serial.write('\x0A')
+    response = serial.read(1)
+    if response == ready_signal:
+        ready = True
+        print "Pump is ready to received buffered command"
+        # Clear buffer
+        response = serial.read(10)
+
+# Enter lock mode
+message_to_write = [chr(ord('U')), '\x0D']
+
+print "Writing: " + str(message_to_write)
+
+for message in message_to_write:
+    received = False
+    while not received:
+        print "Writing: " + message
+        serial.write(message)
+        time.sleep(1)
+        response = serial.read(10)
+        print "Response: " + str((response, ''))
+        if response == message:
+            received = True
+        else:
+            print "Error in transmission of " + str((message, ''))
+
+# See if status has changed
+print "Getting Pump Status"
+serial.write(chr( ord('?')))
+print "Write: " + "?"
+done = False
+complete_message = []
+while not done:
+    response = serial.read(1)
+    #print (response, ord(response))
+    if ord(response) > 128:
+        done = True
+        print "Triggered complete"
+        complete_message.append( chr(ord(response)-128))
+    else:
+        complete_message.append(response)
+        serial.write(chr(6))
+
+print "Received: " + str(complete_message)
+
+
 
 ### Poll pump for ready signal
 ##ready = False
