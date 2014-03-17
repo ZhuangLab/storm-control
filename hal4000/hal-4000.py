0c9da4952e6a61e2eb4badbc1fdc044b6f4316c8
Jeffrey Moffitt <jeffmoffitt@gmail.com>
2014-03-17 14:25:04 -0400

    Fixed minor bug in estimates code in Hal

--- hal4000/hal-4000.py
+++ hal4000/hal-4000.py
@@ -491,7 +491,7 @@ class Window(QtGui.QMainWindow):
                 # Get disk usage and duration
                 if not message.hasError():
                     num_frames = message.getData("length")
-                    parameters = self.parameters_box.getCurrentParameters(message.getData("parameters"))
+                    parameters = self.parameters_box.getParameters(message.getData("parameters"))
                     message.addResponse("duration", num_frames * parameters.kinetic_value)
                     mega_bytes_per_frame = parameters.bytesPerFrame * 1.0/2^20 # convert to megabytes
                     message.addResponse("disk_usage", mega_bytes_per_frame*num_frames)
