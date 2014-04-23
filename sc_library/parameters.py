524ebae0bd6c60764aa74f2b6f6f88752e8415db
Hazen Babcock <HazenBabcock@github.com>
2014-04-23 07:46:49 -0400

    Add additional attributes to the parameters XML object for HAL shutters / illumination.

--- sc_library/parameters.py
+++ sc_library/parameters.py
@@ -163,7 +163,10 @@ def Parameters(parameters_file, is_HAL = False):
         if not os.path.exists(xml_object.shutters):
             xml_object.shutters = os.path.dirname(parameters_file) + "/" + xml_object.shutters
 
+        xml_object.shutter_colors = []
         xml_object.shutter_data = []
+        xml_object.shutter_frames = 0
+
         xml_object.parameters_file = parameters_file
         xml_object.initialized = False
 
