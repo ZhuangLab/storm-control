c43b4618810d75bf55c3d78baae2efdafface0d8
Jeffrey Moffitt <jeffmoffitt@gmail.com>
2015-04-04 10:01:40 -0400

    Added get position button to steve

---
+++ steve/steve_ui.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'steve.ui'
+#
+# Created: Sat Apr 04 10:00:43 2015
+#      by: PyQt4 UI code generator 4.9.6
+#
+# WARNING! All changes made in this file will be lost!
+
