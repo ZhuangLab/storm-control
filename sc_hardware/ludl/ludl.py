5c2d8f23f5bbeddc9e0385139e79a5449a1b83f3
George Emanuel <emanuega0@gmail.com>
2015-06-09 13:25:05 -0400

    Add TCP control
    Added TCP/IP interface for Ludl stage and made serial interface function

--- sc_hardware/ludl/ludl.py
+++ sc_hardware/ludl/ludl.py
@@ -6,39 +6,48 @@
 #
 # Hazen 02/14
 #
+# Edited 5/15 to add Z piezo and TCP/IP protocol
+# George Emanuel 5/15
 
 import sc_hardware.serial.RS232 as RS232
 import time
 
+import httplib
+
 
 ## Ludl
 #
-# Encapsulates control of a XY Ludl stage.
+# Encapsulates control of a XY Ludl stage, communicating through serial.
 #
 class Ludl(RS232.RS232):
 
     ## __init__
     #
-    # @param port (Optional) The RS-232 port to use, defaults to "COM2".
+    # @param port (Optional) The RS-232 port to use, defaults to "COM5".
     # @param timeout (Optional) The time out value for communication, defaults to None.
-    # @param baudrate (Optional) The communication baud rate, defaults to 9600.
+    # @param baudrate (Optional) The communication baud rate, defaults to 115200.
     # @param wait_time How long to wait between polling events before it is decided that there is no new data available on the port, defaults to 20ms.
     #
-    def __init__(self, port, timeout = None, baudrate = 9600, wait_time = 0.02):
+    def __init__(self, port="COM5", timeout = None, baudrate = 115200, wait_time = 0.02):
         self.unit_to_um = 0.2
         self.um_to_unit = 1.0/self.unit_to_um
-        self.x = 0.0
-        self.y = 0.0
 
         # RS232 stuff
         RS232.RS232.__init__(self, port, timeout, baudrate, "\r", wait_time)
 
+	self.live = True
         try:
             test = self.commWithResp("Ver")
         except:
             self.live = False
         if not self.live:
             print "Ludl Stage is not connected? Stage is not on?"
+
+	#Set to analog mode?
+	if (self.live):
+	    self._command("CAN 3, 83, 267, 0")
+	    self._command("stspeed x=500, y=500")
+	    self._command("speed x=1000, y=1000")
 
     ## _command
     #
@@ -50,6 +59,14 @@ class Ludl(RS232.RS232):
         response = self.commWithResp(command)
         if response:
             return response.split("\r")
+
+    ## _commandIgnoreResponse
+    # @Override
+    #
+    # @param command The command string to send
+    #
+    def _commandIgnoreResponse(self, command):
+	self.sendCommand(command)
 
     ## getStatus
     #
@@ -64,11 +81,9 @@ class Ludl(RS232.RS232):
     # @param y Y position in um.
     #
     def goAbsolute(self, x, y):
-        newx = str(round(x * self.um_to_unit))
-        newy = str(round(y * self.um_to_unit))
-        self.sendCommand("Move x=" + newx)
-        self.sendCommand("Move y=" + newy)
-        self.waitResponse()
+        newx = str(int(round(x * self.um_to_unit)))
+        newy = str(int(round(y * self.um_to_unit)))
+        self._commandIgnoreResponse("Move x=" + newx + ",y="+newy)
 
     ## goRelative
     #
@@ -76,11 +91,9 @@ class Ludl(RS232.RS232):
     # @param dy Amount to move in y in um.
     #
     def goRelative(self, dx, dy):
-        newx = str(round(dx * self.um_to_unit))
-        newy = str(round(dy * self.um_to_unit))
-        self.sendCommand("Movrel x=" + newx)
-        self.sendCommand("Movrel y=" + newy)
-        self.waitResponse()
+        newx = str(int(round(dx * self.um_to_unit)))
+        newy = str(int(round(dy * self.um_to_unit)))
+        self._commandIgnoreResponse("Movrel x=" + newx + ",y="+newy)
 
     ## info
     #
@@ -95,8 +108,9 @@ class Ludl(RS232.RS232):
     # @param y_speed Speed the stage should be moving at in y in um/s.
     #
     def jog(self, x_speed, y_speed):
+	print 'not implemented for Ludl stage'
         #print "VS {0:.1f},{1:.1f}".format(x_speed,y_speed)
-        self._command("VS {0:.1f},{1:.1f}".format(x_speed,y_speed))
+        #self._command("VS {0:.1f},{1:.1f}".format(x_speed,y_speed))
 
     ## joystickOnOff
     #
@@ -104,24 +118,26 @@ class Ludl(RS232.RS232):
     #
     def joystickOnOff(self, on):
         if on:
-            self._command("Joystick X+ Y+")
+            self._commandIgnoreResponse("Joystick X+ Y+")
         else:
-            self._command("Joystick X- Y-")
+            self._commandIgnoreResponse("Joystick X- Y-")
      
     ## position
     #
     # @return [stage x (um), stage y (um), stage z (um)].
     #
     def position(self):
-#        try:
-        self.x = float(self._command("Where X")[0].split(" ")[1])
-        self.y = float(self._command("Where Y")[0].split(" ")[1])
-#            [self.x, self.y, self.z] = map(int, response.split(","))
-#        except:
-#            pass
-        return [self.x * self.unit_to_um,
-                self.y * self.unit_to_um,
-                0.0]
+	response = self._command("WHERE X Y B")[0].split(" ")
+	try:
+	    x = int(response[1]) * self.unit_to_um
+	    y = int(response[2]) * self.unit_to_um
+	    z = int(response[3]) * self.unit_to_um
+	except:
+            x = int(response[2]) * self.unit_to_um
+	    y = int(response[3]) * self.unit_to_um
+	    z = int(response[4]) * self.unit_to_um
+
+        return [x, y, z]
 
     ## setVelocity
     #
@@ -129,15 +145,90 @@ class Ludl(RS232.RS232):
     # @param y_vel The maximum stage velocity allowed in y in Ludl units.
     #
     def setVelocity(self, x_vel, y_vel):
-        self._command("Speed x=" + str(x_vel))
-        self._command("Speed y=" + str(y_vel))
+        self._commandIgnoreResponse("Speed x=" + str(x_vel) + ",y=str(y_vel)")
 
     ## zero
     #
     # Set the current position as the stage zero position.
     #
     def zero(self):
-        self._command("Here x=0 y=0")
+        self._commandIgnoreResponse("Here x=0 y=0 b=0")
+
+    ## zMoveTo
+    #
+    # @param z The z value to move to the (piezo) stage to, in microns.
+    #
+    def zMoveTo(self, z):
+        self._commandIgnoreResponse("Move B=" + str(int(round(z * self.um_to_unit))))
+
+    ## zMoveRelative
+    #
+    # @param dz Amount to move piezo (in um).
+    #
+    def zMoveRelative(self, dz):
+        self._commandIgnoreResponse("Moverel B=" + str(int(round(dz * self.um_to_unit))))
+
+    ## zPosition
+    #
+    # @return The current z position of the (piezo) stage, in microns.
+    def zPosition(self):
+        return int(self._command("WHERE B")[0].split(" ")[1])*self.unit_to_um
+
+## LudlTCP
+#
+# Encapsulates control of a XY Ludl stage, communicating through TCP/IP.
+# Ideally this should be done through a more abstract Ludl class, not
+# one that already communicates through serial, but I don't want to break
+# other code.
+#
+class LudlTCP(Ludl):
+
+    ## __init__
+    #
+    # @param ipAddress (Optional) The IP address, defaults to "192.168.100.1"
+    #
+    def __init__(self, ipAddress="192.168.100.1"):
+        self.unit_to_um = 0.2
+        self.um_to_unit = 1.0/self.unit_to_um
+
+	self.connection = httplib.HTTPConnection(ipAddress)
+
+	self.live = True
+        try:
+            test = self._command("Ver")
+        except:
+            self.live = False
+        if not self.live:
+            print "Ludl Stage is not connected? Stage is not on?"
+
+	#Set to analog mode?
+	if (self.live):
+	    self._commandIgnoreResponse("CAN 3, 83, 267, 0")
+	    self._commandIgnoreResponse("stspeed x=500, y=500")
+
+    ## _command
+    # @Override
+    #
+    # @param command The command string to send.
+    #
+    # @return The response to the command.
+    #
+    def _command(self, command):
+	formCommand = "/conajx.asp?&ECMD1=\"" + command + "\""
+	self.connection.request("GET", "/conajx.asp?&ECMD1=\"" + command + "\"")
+	response = self.connection.getresponse().read().split("\r")[2].strip()
+
+	return ["A: " + response]
+
+    ## _commandIgnoreResponse
+    # @Override
+    #
+    # @param command The command string to send
+    #
+    def _commandIgnoreResponse(self, command):
+	t = time.time()
+	self.connection.request("GET", "/conajx.asp?&ECMD1=\"" + command+ "\"")
+	self.connection.getresponse().read()
 
 
 #
@@ -145,7 +236,7 @@ class Ludl(RS232.RS232):
 # 
 
 if __name__ == "__main__":
-    stage = Ludl("COM1")
+    stage = Ludl("COM5")
     print stage.position()
     stage.zero()
     time.sleep(0.1)
