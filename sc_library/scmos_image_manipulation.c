17fa0dc1fbc0bff14884e549df9a9e673ad70812
Hazen Babcock <hbabcock@fas.harvard.edu>
2015-09-23 16:39:57 -0400

    Initial work on modications to handle axis flipping and transposition.

--- sc_library/scmos_image_manipulation.c
+++ sc_library/scmos_image_manipulation.c
@@ -6,36 +6,52 @@
  *
  * Hazen 10/13
  *
- * compilation:
+ * Add additional functions to cover all the orientation possibilities.
+ *
+ * Hazen 09/15
+ *
+ *
+ * Compilation (windows):
  * gcc -c scmos_image_manipulation.c -O3
  * gcc -shared -o scmos_image_manipulation.dll scmos_image_manipulation.o
+ *
+ * Compilation (linux):
+ * gcc -fPIC -g -c -Wall scmos_image_manipulation.c -O3
+ * gcc -shared -Wl,-soname,scmos_image_manipulation.so.1 -o scmos_image_manipulation.so.1.0.1 scmos_image_manipulation.o -lc 
+ * ln -s scmos_image_manipulation.so.1.0.1 scmos_image_manipulation.so
+ *
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /* function definitions */
-void rescaleImage(unsigned char*, unsigned short *, int, int, int, int *, int *);
+void rescaleImage000(unsigned char*, unsigned short *, int, int, int, int, int *, int *);
 
 /* functions */
 
-/* rescaleImage
+/* rescaleImage000
  *
  * Converts to thresholded 8 bit for Qt.
  *
  * @param scaled_image Storage for the scaled image.
  * @param image The original image data from the camera, assumed to be 16 bit.
- * @param image_size The number of pixels in the image.
+ * @param image_width The width of the image (the "slow" dimension).
+ * @param image_height The height of the image (the "fast" dimension).
  * @param display_min The value in image that will be zero in the scaled image.
  * @param display_max The value in image that will be 255 in the scaled image.
  * @param image_min The minimum value in image.
  * @param image_max The maxiumum value in image.
  */
-void rescaleImage(unsigned char * scaled_image, unsigned short *image, int image_size, int display_min, int display_max, int *image_min, int *image_max)
+void rescaleImage000(unsigned char * scaled_image, unsigned short *image, int image_width, int image_height, int display_min, int display_max, int *image_min, int *image_max)
 {
-  int cur_min,cur_max,i;
+  int cur_min,cur_max,i,image_size;
   double min,scale,temp;
 
   min = (double)display_min;
   scale = 255.0/((double)(display_max - display_min));
 
+  image_size = image_width * image_height;
   cur_min = image[0];
   cur_max = image[0];
   for(i=0;i<image_size;i++){
@@ -61,11 +77,94 @@ void rescaleImage(unsigned char * scaled_image, unsigned short *image, int image
   *image_min = cur_min;
   *image_max = cur_max;
 }
-			      
+
+/* Transpose */
+void rescaleImage001(unsigned char * scaled_image, unsigned short *image, int image_width, int image_height, int display_min, int display_max, int *image_min, int *image_max)
+{
+  int cur_min,cur_max,i,j,ij;
+  double min,scale,temp;
+
+  min = (double)display_min;
+  scale = 255.0/((double)(display_max - display_min));
+
+  printf("%d %d\n", image_width, image_height);
+
+  cur_min = image[0];
+  cur_max = image[0];
+  for(i=0;i<image_width;i++){
+    for(j=0;j<image_height;j++){
+
+      ij = i*image_height + j;
+      
+      if(image[ij]<cur_min){
+	cur_min = image[ij];
+      }
+      else if(image[ij]>cur_max){
+	cur_max = image[ij];
+      }
+
+      temp = (double)image[ij] - min;
+      temp = temp*scale;
+      if(temp < 0.0){
+	temp = 0.0;
+      }
+      else if(temp > 255.0){
+	temp = 255.0;
+      }
+      scaled_image[j*image_width+i] = (unsigned char)(temp + 0.5);
+    }
+  }
+
+  *image_min = cur_min;
+  *image_max = cur_max;
+}
+
+/* Flip Vertical */
+void rescaleImage010(unsigned char * scaled_image, unsigned short *image, int image_width, int image_height, int display_min, int display_max, int *image_min, int *image_max)
+{
+  int cur_min,cur_max,i,j,ij;
+  double min,scale,temp;
+
+  min = (double)display_min;
+  scale = 255.0/((double)(display_max - display_min));
+
+  printf("%d %d\n", image_width, image_height);
+
+  cur_min = image[0];
+  cur_max = image[0];
+  for(i=0;i<image_width;i++){
+    for(j=0;j<image_height;j++){
+
+      ij = i*image_height + j;
+      
+      if(image[ij]<cur_min){
+	cur_min = image[ij];
+      }
+      else if(image[ij]>cur_max){
+	cur_max = image[ij];
+      }
+
+      temp = (double)image[ij] - min;
+      temp = temp*scale;
+      if(temp < 0.0){
+	temp = 0.0;
+      }
+      else if(temp > 255.0){
+	temp = 255.0;
+      }
+      scaled_image[i*image_height+(image_height-j-1)] = (unsigned char)(temp + 0.5);
+    }
+  }
+
+  *image_min = cur_min;
+  *image_max = cur_max;
+}
+
+
 /*
  * The MIT License
  *
- * Copyright (c) 2013 Zhuang Lab, Harvard University
+ * Copyright (c) 2015 Zhuang Lab, Harvard University
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- sc_library/scmos_image_manipulation_c.py
+++ sc_library/scmos_image_manipulation_c.py
@@ -7,7 +7,7 @@
 # was that for the largest images using numpy to do the image scaling
 # and typy conversion was not fast enough.
 #
-# Hazen 10/13
+# Hazen 09/15
 # 
 
 import ctypes
@@ -15,21 +15,32 @@ import math
 import numpy
 from numpy.ctypeslib import ndpointer
 import os
+import sys
 
 directory = os.path.dirname(__file__)
-if not (directory == ""):
+if (directory == ""):
+    directory = "./"
+else:
     directory += "/"
 
-image_manip = ctypes.cdll.LoadLibrary(directory + "scmos_image_manipulation.dll")
+if (sys.platform == "win32"):
+    image_manip = ctypes.cdll.LoadLibrary(directory + "scmos_image_manipulation.dll")
+else:
+    image_manip = ctypes.cdll.LoadLibrary(directory + "scmos_image_manipulation.so")
 
 # C interface definition.
-image_manip.rescaleImage.argtypes = [ndpointer(dtype=numpy.uint8),
-                                     ndpointer(dtype=numpy.uint16),
-                                     ctypes.c_int,
-                                     ctypes.c_int,
-                                     ctypes.c_int,
-                                     ctypes.c_void_p,
-                                     ctypes.c_void_p]
+rescale_fn_arg_types = [ndpointer(dtype=numpy.uint8),
+                        ndpointer(dtype=numpy.uint16),
+                        ctypes.c_int,
+                        ctypes.c_int,
+                        ctypes.c_int,
+                        ctypes.c_int,
+                        ctypes.c_void_p,
+                        ctypes.c_void_p]
+image_manip.rescaleImage000.argtypes = rescale_fn_arg_types
+image_manip.rescaleImage001.argtypes = rescale_fn_arg_types
+image_manip.rescaleImage010.argtypes = rescale_fn_arg_types
+
 
 ## rescaleImage
 #
@@ -38,35 +49,66 @@ image_manip.rescaleImage.argtypes = [ndpointer(dtype=numpy.uint8),
 # in the image.
 #
 # @param image The original image as numpy.uint16 array.
+# @param flip_h Flip horizontal.
+# @param flip_v Flip vertical.
+# @param transpose Transpose image.
 # @param display_range [image value that equals 0, image value that equals 255].
 #
-# @return [numpy.uint8 image, original image minimum, original image maximum]
+# @return [numpy.uint8 image, original image minimum, original image maximum, transposed dimensions]
 #
-def rescaleImage(image, display_range):
-    c_rescale = numpy.empty(image.shape, dtype = numpy.uint8)
+def rescaleImage(image, flip_h, flip_v, transpose, display_range):
+
+    # Create a string specifying the operations that will be performed on the image.
+    op_code = ""
+    for op in [flip_h, flip_v, transpose]:
+        if op:
+            op_code += "1"
+        else:
+            op_code += "0"
+
+    print "op:", op_code
+
+    # Any of these operations will change the final dimensions of the image.
+    if (op_code in ["001", "011", "101", "111"]):
+        transposed_dimensions = True
+        c_rescale = numpy.empty((image.shape[1], image.shape[0]), dtype = numpy.uint8)
+    else:
+        transposed_dimensions = False        
+        c_rescale = numpy.empty((image.shape[0], image.shape[1]), dtype = numpy.uint8)
+
     image_min = ctypes.c_int(0)
     image_max = ctypes.c_int(0)
-    image_manip.rescaleImage(c_rescale,
-                             image,
-                             image.size,
-                             display_range[0],
-                             display_range[1],
-                             ctypes.byref(image_min),
-                             ctypes.byref(image_max))
 
-    return [c_rescale, image_min.value, image_max.value]
+    # Get the appropriate C function based on the op_code.
+    image_fn = image_manip.__dict__["rescaleImage" + op_code]
+
+    image_fn(c_rescale,
+             image,
+             image.shape[0],
+             image.shape[1],
+             display_range[0],
+             display_range[1],
+             ctypes.byref(image_min),
+             ctypes.byref(image_max))
+
+    return [c_rescale, image_min.value, image_max.value, transposed_dimensions]
 
 # Testing
-if __name__ == "__main__":
-    image = numpy.ones((256,256), dtype = numpy.uint16)
-    image[10,10] = 10
-    [rs_image, image_min, image_max] = rescaleImage(image, [2, 9])
-    print rs_image[0,0], rs_image[10,10], image_min, image_max
+if (__name__ == "__main__"):
+
+    from PIL import Image
+
+    im = Image.open("raw_image.png")
+    nim = numpy.ascontiguousarray(numpy.amax(numpy.array(im), 2).astype(numpy.uint16))
+
+    [rs_nim, image_min, image_max, transposed_dimensions] = rescaleImage(nim, False, True, False, [0, 255])
+    fim = Image.fromarray(rs_nim)
+    fim.save("out.png")
 
 #
 # The MIT License
 #
-# Copyright (c) 2013 Zhuang Lab, Harvard University
+# Copyright (c) 2015 Zhuang Lab, Harvard University
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
