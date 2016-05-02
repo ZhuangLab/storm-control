9b20e62f6a959fa965ab93d5deac498eb96e8ea7
Hazen Babcock <hbabcock@fas.harvard.edu>
2016-05-02 17:25:13 -0400

    Got power button editor working. Saving and loading is still missing.

--- hal4000/illumination/illuminationControl.py
+++ hal4000/illumination/illuminationControl.py
@@ -8,19 +8,19 @@
 #
 
 from PyQt4 import QtCore, QtGui
-
 import qtWidgets.qtAppIcon as qtAppIcon
-import halLib.halModule as halModule
-import illumination.xmlParser as xmlParser
-import illumination.illuminationChannel as illuminationChannel
+
 import sc_library.halExceptions as halExceptions
-import time
+import sc_library.hdebug as hdebug
 import sc_library.parameters as params
 
-# Debugging
-import sc_library.hdebug as hdebug
+import halLib.halModule as halModule
 
-# UIs.
+import illumination.buttonEditor as buttonEditor
+import illumination.illuminationChannel as illuminationChannel
+import illumination.xmlParser as xmlParser
+
+# UI.
 import qtdesigner.illumination_ui as illuminationUi
 
 ## IlluminationControl
@@ -76,10 +76,20 @@ class IlluminationControl(QtGui.QDialog, halModule.HalModule):
         self.parameters.set("illumination.default_power", default_power)
         self.parameters.set("illumination.on_off_state", on_off_state)
 
+        #
+        # FIXME: We also need to be able to save and load these.
+        #
         buttons = []
         for i in range(len(hardware.channels)):
             buttons.append([["Max", 1.0], ["Low", 0.1]])
-        self.parameters.set("illumination.power_buttons", buttons)
+        power_buttons = params.ParameterCustom("Illumination power buttons",
+                                               "power_buttons",
+                                               buttons,
+                                               1,
+                                               is_mutable = True,
+                                               is_saved = False)
+        power_buttons.editor = buttonEditor.ParametersTablePowerButtonEditor
+        self.parameters.add("illumination.power_buttons", power_buttons)
 
         # This parameter is used to be able to tell when the shutters file
         # has been changed for a given set of parameters.
@@ -101,6 +111,7 @@ class IlluminationControl(QtGui.QDialog, halModule.HalModule):
 
         # Illumination channels setup.
         x = 7
+        names = []
         for i, channel in enumerate(hardware.channels):
             a_instance = illuminationChannel.Channel(i,
                                                      channel,
@@ -109,6 +120,9 @@ class IlluminationControl(QtGui.QDialog, halModule.HalModule):
                                                      self.ui.powerControlBox)
             x += a_instance.setPosition(x, 14) + self.spacing
             self.channels.append(a_instance)
+            names.append(a_instance.getName())
+            
+        power_buttons.channel_names = names
 
         # Connect signals.
         if self.have_parent:
@@ -389,6 +403,7 @@ class IlluminationControl(QtGui.QDialog, halModule.HalModule):
         lb_height = self.ui.powerControlBox.height()
         self.ui.okButton.setGeometry(lb_width - 65, lb_height + 4, 75, 24)
         self.setFixedSize(lb_width + 18, lb_height + 36)
+
 
 #
 # The MIT License
