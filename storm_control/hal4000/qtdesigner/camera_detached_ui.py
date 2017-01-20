581285694ddd330a4c913f2a5998fecbe71bb898
Hazen Babcock <hbabcock@mac.com>
2017-01-20 16:51:50 -0500

    Convert the rest of the .ui files to PyQt5.

--- storm_control/hal4000/qtdesigner/camera_detached_ui.py
+++ storm_control/hal4000/qtdesigner/camera_detached_ui.py
@@ -2,98 +2,86 @@
 
 # Form implementation generated from reading ui file 'camera-detached.ui'
 #
-# Created: Sat Sep 19 21:02:02 2015
-#      by: PyQt4 UI code generator 4.11.3
+# Created by: PyQt5 UI code generator 5.7
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
-        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.setObjectName("Dialog")
         Dialog.resize(586, 619)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
         Dialog.setSizePolicy(sizePolicy)
         Dialog.setMinimumSize(QtCore.QSize(0, 0))
         Dialog.setMaximumSize(QtCore.QSize(10000, 10000))
-        self.verticalLayout = QtGui.QVBoxLayout(Dialog)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setSpacing(0)
-        self.verticalLayout.setMargin(0)
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.cameraFrame = QtGui.QFrame(Dialog)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.cameraFrame = QtWidgets.QFrame(Dialog)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.cameraFrame.sizePolicy().hasHeightForWidth())
         self.cameraFrame.setSizePolicy(sizePolicy)
         self.cameraFrame.setMinimumSize(QtCore.QSize(585, 543))
         self.cameraFrame.setMaximumSize(QtCore.QSize(10000, 10000))
-        self.cameraFrame.setFrameShape(QtGui.QFrame.Box)
-        self.cameraFrame.setFrameShadow(QtGui.QFrame.Raised)
-        self.cameraFrame.setObjectName(_fromUtf8("cameraFrame"))
+        self.cameraFrame.setFrameShape(QtWidgets.QFrame.Box)
+        self.cameraFrame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.cameraFrame.setObjectName("cameraFrame")
         self.verticalLayout.addWidget(self.cameraFrame)
-        self.widget = QtGui.QWidget(Dialog)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
+        self.widget = QtWidgets.QWidget(Dialog)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.widget.sizePolicy().hasHeightForWidth())
         self.widget.setSizePolicy(sizePolicy)
         self.widget.setMaximumSize(QtCore.QSize(16777215, 75))
-        self.widget.setObjectName(_fromUtf8("widget"))
-        self.horizontalLayout = QtGui.QHBoxLayout(self.widget)
+        self.widget.setObjectName("widget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout.setContentsMargins(2, 2, 2, 2)
         self.horizontalLayout.setSpacing(2)
-        self.horizontalLayout.setMargin(2)
-        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.cameraParamsFrame = QtGui.QFrame(self.widget)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Preferred)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.cameraParamsFrame = QtWidgets.QFrame(self.widget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.cameraParamsFrame.sizePolicy().hasHeightForWidth())
         self.cameraParamsFrame.setSizePolicy(sizePolicy)
         self.cameraParamsFrame.setMinimumSize(QtCore.QSize(0, 0))
         self.cameraParamsFrame.setMaximumSize(QtCore.QSize(16777215, 1000))
-        self.cameraParamsFrame.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.cameraParamsFrame.setFrameShadow(QtGui.QFrame.Raised)
-        self.cameraParamsFrame.setObjectName(_fromUtf8("cameraParamsFrame"))
+        self.cameraParamsFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.cameraParamsFrame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.cameraParamsFrame.setObjectName("cameraParamsFrame")
         self.horizontalLayout.addWidget(self.cameraParamsFrame)
-        self.widget_3 = QtGui.QWidget(self.widget)
+        self.widget_3 = QtWidgets.QWidget(self.widget)
         self.widget_3.setMinimumSize(QtCore.QSize(100, 0))
         self.widget_3.setMaximumSize(QtCore.QSize(16777215, 92))
-        self.widget_3.setObjectName(_fromUtf8("widget_3"))
-        self.verticalLayout_2 = QtGui.QVBoxLayout(self.widget_3)
-        self.verticalLayout_2.setMargin(2)
-        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
-        spacerItem = QtGui.QSpacerItem(20, 38, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.widget_3.setObjectName("widget_3")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.widget_3)
+        self.verticalLayout_2.setContentsMargins(2, 2, 2, 2)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        spacerItem = QtWidgets.QSpacerItem(20, 38, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.verticalLayout_2.addItem(spacerItem)
-        self.okButton = QtGui.QPushButton(self.widget_3)
+        self.okButton = QtWidgets.QPushButton(self.widget_3)
         self.okButton.setMaximumSize(QtCore.QSize(100, 16777215))
-        self.okButton.setObjectName(_fromUtf8("okButton"))
+        self.okButton.setObjectName("okButton")
         self.verticalLayout_2.addWidget(self.okButton)
         self.horizontalLayout.addWidget(self.widget_3)
         self.verticalLayout.addWidget(self.widget)
+        self.widget.raise_()
+        self.cameraFrame.raise_()
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
-        self.okButton.setText(_translate("Dialog", "Close", None))
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.okButton.setText(_translate("Dialog", "Close"))
 
--- storm_control/hal4000/qtdesigner/spotcounter_ui.py
+++ storm_control/hal4000/qtdesigner/spotcounter_ui.py
@@ -2,103 +2,89 @@
 
 # Form implementation generated from reading ui file 'spotcounter.ui'
 #
-# Created: Fri Nov 22 08:34:09 2013
-#      by: PyQt4 UI code generator 4.9.6
+# Created by: PyQt5 UI code generator 5.7
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
-        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.setObjectName("Dialog")
         Dialog.resize(561, 640)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
         Dialog.setSizePolicy(sizePolicy)
         Dialog.setMinimumSize(QtCore.QSize(561, 640))
         Dialog.setMaximumSize(QtCore.QSize(561, 640))
-        self.okButton = QtGui.QPushButton(Dialog)
+        self.okButton = QtWidgets.QPushButton(Dialog)
         self.okButton.setGeometry(QtCore.QRect(480, 610, 75, 24))
-        self.okButton.setObjectName(_fromUtf8("okButton"))
-        self.tabWidget = QtGui.QTabWidget(Dialog)
+        self.okButton.setObjectName("okButton")
+        self.tabWidget = QtWidgets.QTabWidget(Dialog)
         self.tabWidget.setGeometry(QtCore.QRect(10, 10, 541, 591))
-        self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
-        self.countsTab = QtGui.QWidget()
-        self.countsTab.setObjectName(_fromUtf8("countsTab"))
-        self.graphFrame = QtGui.QFrame(self.countsTab)
+        self.tabWidget.setObjectName("tabWidget")
+        self.countsTab = QtWidgets.QWidget()
+        self.countsTab.setObjectName("countsTab")
+        self.graphFrame = QtWidgets.QFrame(self.countsTab)
         self.graphFrame.setGeometry(QtCore.QRect(60, 40, 471, 371))
-        self.graphFrame.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.graphFrame.setFrameShadow(QtGui.QFrame.Sunken)
-        self.graphFrame.setObjectName(_fromUtf8("graphFrame"))
-        self.countsText1 = QtGui.QLabel(self.countsTab)
+        self.graphFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.graphFrame.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.graphFrame.setObjectName("graphFrame")
+        self.countsText1 = QtWidgets.QLabel(self.countsTab)
         self.countsText1.setGeometry(QtCore.QRect(20, 10, 101, 16))
-        self.countsText1.setObjectName(_fromUtf8("countsText1"))
-        self.countsLabel1 = QtGui.QLabel(self.countsTab)
+        self.countsText1.setObjectName("countsText1")
+        self.countsLabel1 = QtWidgets.QLabel(self.countsTab)
         self.countsLabel1.setGeometry(QtCore.QRect(130, 10, 61, 16))
         self.countsLabel1.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
-        self.countsLabel1.setObjectName(_fromUtf8("countsLabel1"))
-        self.label = QtGui.QLabel(self.countsTab)
+        self.countsLabel1.setObjectName("countsLabel1")
+        self.label = QtWidgets.QLabel(self.countsTab)
         self.label.setGeometry(QtCore.QRect(100, 480, 341, 20))
         self.label.setAlignment(QtCore.Qt.AlignCenter)
-        self.label.setObjectName(_fromUtf8("label"))
-        self.minSpinBox = QtGui.QSpinBox(self.countsTab)
+        self.label.setObjectName("label")
+        self.minSpinBox = QtWidgets.QSpinBox(self.countsTab)
         self.minSpinBox.setGeometry(QtCore.QRect(5, 400, 51, 22))
         self.minSpinBox.setMaximum(1000)
         self.minSpinBox.setSingleStep(10)
-        self.minSpinBox.setObjectName(_fromUtf8("minSpinBox"))
-        self.maxSpinBox = QtGui.QSpinBox(self.countsTab)
+        self.minSpinBox.setObjectName("minSpinBox")
+        self.maxSpinBox = QtWidgets.QSpinBox(self.countsTab)
         self.maxSpinBox.setGeometry(QtCore.QRect(5, 30, 51, 22))
         self.maxSpinBox.setMinimum(10)
         self.maxSpinBox.setMaximum(1000)
         self.maxSpinBox.setSingleStep(10)
         self.maxSpinBox.setProperty("value", 1000)
-        self.maxSpinBox.setObjectName(_fromUtf8("maxSpinBox"))
-        self.tabWidget.addTab(self.countsTab, _fromUtf8(""))
-        self.imageTab = QtGui.QWidget()
-        self.imageTab.setObjectName(_fromUtf8("imageTab"))
-        self.imageFrame = QtGui.QFrame(self.imageTab)
+        self.maxSpinBox.setObjectName("maxSpinBox")
+        self.tabWidget.addTab(self.countsTab, "")
+        self.imageTab = QtWidgets.QWidget()
+        self.imageTab.setObjectName("imageTab")
+        self.imageFrame = QtWidgets.QFrame(self.imageTab)
         self.imageFrame.setGeometry(QtCore.QRect(10, 40, 516, 516))
-        self.imageFrame.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.imageFrame.setFrameShadow(QtGui.QFrame.Sunken)
-        self.imageFrame.setObjectName(_fromUtf8("imageFrame"))
-        self.countsText2 = QtGui.QLabel(self.imageTab)
+        self.imageFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.imageFrame.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.imageFrame.setObjectName("imageFrame")
+        self.countsText2 = QtWidgets.QLabel(self.imageTab)
         self.countsText2.setGeometry(QtCore.QRect(20, 10, 101, 16))
-        self.countsText2.setObjectName(_fromUtf8("countsText2"))
-        self.countsLabel2 = QtGui.QLabel(self.imageTab)
+        self.countsText2.setObjectName("countsText2")
+        self.countsLabel2 = QtWidgets.QLabel(self.imageTab)
         self.countsLabel2.setGeometry(QtCore.QRect(130, 10, 61, 16))
         self.countsLabel2.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
-        self.countsLabel2.setObjectName(_fromUtf8("countsLabel2"))
-        self.tabWidget.addTab(self.imageTab, _fromUtf8(""))
+        self.countsLabel2.setObjectName("countsLabel2")
+        self.tabWidget.addTab(self.imageTab, "")
 
         self.retranslateUi(Dialog)
         self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(_translate("Dialog", "HAL-4000 Spot Counter", None))
-        self.okButton.setText(_translate("Dialog", "Ok", None))
-        self.countsText1.setText(_translate("Dialog", "Total Localizations:", None))
-        self.countsLabel1.setText(_translate("Dialog", "TextLabel", None))
-        self.label.setText(_translate("Dialog", "This Space Intentionally Left Blank", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.countsTab), _translate("Dialog", "Counts", None))
-        self.countsText2.setText(_translate("Dialog", "Total Localizations:", None))
-        self.countsLabel2.setText(_translate("Dialog", "TextLabel", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.imageTab), _translate("Dialog", "STORM Image", None))
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "HAL-4000 Spot Counter"))
+        self.okButton.setText(_translate("Dialog", "Ok"))
+        self.countsText1.setText(_translate("Dialog", "Total Localizations:"))
+        self.countsLabel1.setText(_translate("Dialog", "TextLabel"))
+        self.label.setText(_translate("Dialog", "This Space Intentionally Left Blank"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.countsTab), _translate("Dialog", "Counts"))
+        self.countsText2.setText(_translate("Dialog", "Total Localizations:"))
+        self.countsLabel2.setText(_translate("Dialog", "TextLabel"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.imageTab), _translate("Dialog", "STORM Image"))
 
