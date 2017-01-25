c43c9ca55ef96e088fa8ce5e666b4972e11d18f3
Hazen Babcock <hbabcock@mac.com>
2017-01-25 12:19:42 -0500

    Add a test for zee-calibrator.

---
+++ storm_control/test/test_zeecal_starts.py
@@ -0,0 +1,20 @@
+#
+# A very simple test of zee-calibrator.
+#
+
+from PyQt5 import QtCore
+import time
+
+import pytestqt
+
+import storm_control.zee_calibrator.main as zcal
+
+def test_zeecal_starts(qtbot):
+
+    mainw = zcal.Window()
+    mainw.show()
+
+    qtbot.addWidget(mainw)
+
+    # Run for about 0.5 seconds.
+    qtbot.wait(500)
