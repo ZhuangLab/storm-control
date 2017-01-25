940bc301d892cc432258b502b155f269214f2473
Hazen Babcock <hbabcock@mac.com>
2017-01-25 10:21:10 -0500

    Get HAL to start without requiring a file named 'shutters_default.xml'

--- storm_control/test/test_hal_starts.py
+++ storm_control/test/test_hal_starts.py
@@ -13,17 +13,20 @@ import storm_control.sc_library.parameters as params
 def test_hal_starts(qtbot):
 
     # Load 'none' hardware configuration.
-    none_hardware = params.hardware("./hal_xml/none_hardware.xml")
+    none_hardware = params.hardware("./hal_xml/test_hardware.xml")
 
     # Load general parameters.
-    general_parameters = params.halParameters("./hal_xml/settings_default.xml")
+    general_parameters = params.halParameters("./hal_xml/test_settings.xml")
 
     # Start HAL.
     hal = hal4000.Window(none_hardware, general_parameters)
     
     params.setDefaultParameters(general_parameters)
 
-    hal.parameters_box.addParameters(general_parameters)
+    # Load parameters with the correct shutters file,
+    shutters_parameters = params.halParameters("./hal_xml/set_shutters.xml")
+
+    hal.parameters_box.addParameters(shutters_parameters)
     hal.toggleSettings()
     hal.show()
 
