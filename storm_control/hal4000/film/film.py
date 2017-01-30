562839d94281873a2a8623ed893a9055fca53fb0
Hazen Babcock <hbabcock@mac.com>
2017-01-30 14:08:41 -0500

    Initial commit.

---
+++ storm_control/hal4000/film/film.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""
+
+Handles filming.
+
+This module is responsible for everything related to filming,
+including starting and stopping the cameras, saving the frames,
+etc..
+
+Much of the logic in the Python2/PyQt4 HAL is now located in
+this module.
+
+Hazen 01/17
+"""
+
+from PyQt5 import QtWidgets
+
+import storm_control.hal4000.halLib.halMessage as halMessage
+import storm_control.hal4000.halLib.halModule as halModule
+import storm_control.hal4000.qtdesigner.film_ui as filmUi
+
+
+class FilmBox(QtWidgets.QGroupBox):
+
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
+
+        self.ui = filmUi.Ui_GroupBox()
+        self.ui.setupUi(self)
+    
+class Film(halModule.HalModule):
+
+    def __init__(self, module_params = None, qt_settings = None, **kwds):
+        super().__init__(**kwds)
+
+        self.ui_parent = module_params.get("ui_parent")
+
+        self.view = FilmBox()
+
+    def processMessage(self, message):
+        super().processMessage(message)
+        if (message.level == 1):
+            if (message.m_type == "configure"):
+                self.newMessage.emit(halMessage.HalMessage(source = self,
+                                                           m_type = "add to ui",
+                                                           data = {"ui_parent" : self.ui_parent,
+                                                                   "ui_widget" : self.view}))
+
