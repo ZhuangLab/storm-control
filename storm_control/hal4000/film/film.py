c9cda37eae290ea488f22b4c4c131a88c0bc1aa1
Hazen Babcock <hbabcock@mac.com>
2017-03-22 23:55:58 -0400

    Restore most of the GUI functionality.

--- storm_control/hal4000/film/film.py
+++ storm_control/hal4000/film/film.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 """
-
 Handles filming.
 
 This module is responsible for everything related to filming,
 including starting and stopping the cameras, saving the frames,
 etc..
 
-Much of the logic in the Python2/PyQt4 HAL is now located in
-this module.
+Much of the logic in the main part of Python2/PyQt4 HAL is 
+now located in this module.
 
 Hazen 01/17
 """
+
+import os
 
 from PyQt5 import QtWidgets
 
@@ -23,27 +24,14 @@ import storm_control.hal4000.qtdesigner.film_ui as filmUi
 
 
 class FilmBox(QtWidgets.QGroupBox):
-
-    def __init__(self, **kwds):
+    """
+    The UI.
+    """
+    def __init__(self, parameters = None, **kwds):
         super().__init__(**kwds)
+        self.parameters = parameters
 
-        self.ui = filmUi.Ui_GroupBox()
-        self.ui.setupUi(self)
-
-    def updateUI(self, parameters):
-        pass
-    
-    
-class Film(halModule.HalModule):
-
-    def __init__(self, module_params = None, qt_settings = None, **kwds):
-        super().__init__(**kwds)
-
-        self.logfile_fp = open(module_params.get("directory") + "image_log.txt", "a")
-
-        self.parameters = module_params.getp("parameters")
-
-        # Hardwired default film settings.
+        # Add default film parameters.        
         self.parameters.add("acq_mode", params.ParameterSetString("Acquisition mode",
                                                                   "acq_mode",
                                                                   "fixed_length",
@@ -57,10 +45,6 @@ class Film(halModule.HalModule):
                                                                         "auto_shutters",
                                                                         True))
         
-        self.parameters.add("directory", params.ParameterStringDirectory("Current working directory",
-                                                                         "directory",
-                                                                         module_params.get("directory")))
-        
         self.parameters.add("filename", params.ParameterString("Current movie file name",
                                                                "filename",
                                                                "movie"))
@@ -68,15 +52,13 @@ class Film(halModule.HalModule):
         self.parameters.add("filetype", params.ParameterSetString("Movie file type",
                                                                   "filetype",
                                                                   ".dax",
-                                                                  [".dax", ".tiff"]))
+                                                                  [".dax", ".tif"]))
         
         self.parameters.add("frames", params.ParameterRangeInt("Movie length in frames",
                                                                "frames",
                                                                10,
                                                                1,
                                                                1000000000))
-                                                               
-        #self.parameters.add("logfile", params)
         
         self.parameters.add("want_bell", params.ParameterSetBoolean("Sound bell at the end of long movies",
                                                                     "want_bell",
@@ -85,9 +67,120 @@ class Film(halModule.HalModule):
         self.parameters.add("dax_big_endian", params.ParameterSetBoolean("Save .dax movies using a big endian format",
                                                                          "dax_big_endian",
                                                                          False))
+
+        # Initial UI configuration.
+        self.ui = filmUi.Ui_GroupBox()
+        self.ui.setupUi(self)
         
+        for extname in self.parameters.getp("extension").getAllowed():
+            self.ui.extensionComboBox.addItem(extname)
         
-        self.view = FilmBox()
+        for typename in self.parameters.getp("filetype").getAllowed():
+            self.ui.filetypeComboBox.addItem(typename)
+
+        self.ui.framesText.setText("")
+        self.ui.sizeText.setText("")
+
+        self.setDirectory(self.parameters.get("directory"))
+        self.setShutters("NA")
+        self.newParameters(self.parameters)
+        self.updateFilenameLabel()
+
+        # Connect signals
+        self.ui.autoIncCheckBox.stateChanged.connect(self.handleAutoInc)
+        self.ui.autoShuttersCheckBox.stateChanged.connect(self.handleAutoShutters)
+        self.ui.extensionComboBox.currentIndexChanged.connect(self.handleExtension)
+        self.ui.filenameEdit.textChanged.connect(self.handleFilename)
+        self.ui.filetypeComboBox.currentIndexChanged.connect(self.handleFiletype)
+        self.ui.indexSpinBox.valueChanged.connect(self.handleIndex)
+        self.ui.lengthSpinBox.valueChanged.connect(self.handleLength)
+        self.ui.modeComboBox.currentIndexChanged.connect(self.handleMode)
+
+    def handleAutoInc(self, state):
+        self.parameters.set("auto_increment", state)
+
+    def handleAutoShutters(self, state):
+        self.parameters.set("auto_shutters", state) 
+
+    def handleExtension(self, index):
+        self.parameters.set("extension", self.ui.extensionComboBox.currentText())
+        self.updateFilenameLabel()
+
+    def handleFilename(self):
+        self.parameters.set("filename", str(self.ui.filenameEdit.displayText()))
+        self.updateFilenameLabel()
+
+    def handleFiletype(self, index):
+        self.parameters.set("filetype", str(self.ui.filetypeComboBox.currentText()))
+        self.updateFilenameLabel()
+        
+    def handleIndex(self, index):
+        self.updateFilenameLabel()
+
+    def handleLength(self, index):
+        self.parameters.set("frames", index)
+
+    def handleMode(self, index):
+        print("hm", index)
+        if (index == 0):
+            self.parameters.set("acq_mode", "run_till_abort")
+            self.ui.lengthSpinBox.hide()
+        else:
+            self.parameters.set("acq_mode", "fixed_length")
+            self.ui.lengthSpinBox.show()
+        
+    def newParameters(self, parameters):
+        self.ui.autoIncCheckBox.setChecked(parameters.get("auto_increment"))
+        self.ui.autoShuttersCheckBox.setChecked(parameters.get("auto_shutters"))
+        self.ui.extensionComboBox.setCurrentIndex(self.ui.extensionComboBox.findText(parameters.get("extension")))
+        self.ui.filenameEdit.setText(parameters.get("filename"))
+        self.ui.filetypeComboBox.setCurrentIndex(self.ui.filetypeComboBox.findText(parameters.get("filetype")))
+        self.ui.lengthSpinBox.setValue(parameters.get("frames"))
+        
+        if (parameters.get("acq_mode") == "run_till_abort"):
+            self.ui.modeComboBox.setCurrentIndex(0)
+        else:
+            self.ui.modeComboBox.setCurrentIndex(1)
+
+    def setDirectory(self, new_directory):
+        self.parameters.set("directory", new_directory)
+        self.ui.directoryText.setText("  " + new_directory[-30:])
+
+    def setShutters(self, new_shutters):
+        self.ui.shuttersText.setText("  " + new_shutters)
+
+    def updateFilenameLabel(self):
+        name = self.parameters.get("filename")
+        name += "_{0:04d}".format(self.ui.indexSpinBox.value())
+        if len(self.parameters.get("extension")) > 0:
+            name += "_" + self.parameters.get("extension")
+        name += self.parameters.get("filetype")
+
+        self.ui.filenameLabel.setText(name)
+        if os.path.exists(os.path.join(self.parameters.get("directory"), name)):
+            self.will_overwrite = True
+            self.ui.filenameLabel.setStyleSheet("QLabel { color: red}")
+        else:
+            self.will_overwrite = False
+            self.ui.filenameLabel.setStyleSheet("QLabel { color: black}")
+        
+
+class Film(halModule.HalModuleBuffered):
+
+    def __init__(self, module_params = None, qt_settings = None, **kwds):
+        super().__init__(**kwds)
+
+        self.logfile_fp = open(module_params.get("directory") + "image_log.txt", "a")
+
+        p = module_params.getp("parameters")
+        p.add("directory", params.ParameterStringDirectory("Current working directory",
+                                                           "directory",
+                                                           module_params.get("directory"),
+                                                           is_mutable = False,
+                                                           is_saved = False))
+                
+        self.view = FilmBox(parameters = p)
+
         self.configure_dict = {"ui_order" : 1,
                                "ui_parent" : "hal.containerWidget",
                                "ui_widget" : self.view}
