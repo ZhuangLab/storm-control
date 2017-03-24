0972e1442689507519050cdd64822f501d179389
Hazen Babcock <hbabcock@mac.com>
2017-03-24 06:33:08 -0400

    Add binning information to the .dax file so that Insight3 can read it.

--- storm_control/hal4000/halLib/imagewriters.py
+++ storm_control/hal4000/halLib/imagewriters.py
@@ -53,67 +53,65 @@ def createFileWriter(feed_params, film_settings):
 # @param stage_position The stage position, [stage x, stage y, stage z].
 # @param lock_target The focus lock target.
 #
-def writeInfFile(filename, number_frames, parameters, camera):
-    c = camera
-    fp = open(filename + ".inf", "w")
-    nl = "\n"
-    p = parameters
-
-    # General info
-    fp.write("information file for" + nl)
-    fp.write(filename + nl)
-    fp.write("software version = " + software_version + nl)
-    fp.write("machine name = " + p.get("setup_name") + nl)
-    fp.write("parameters file = " + p.get("parameters_file") + nl)
-    fp.write("shutters file = " + p.get("illumination.shutters", "NA") + nl)
-    if p.get("film.want_big_endian"):
-        fp.write("data type = 16 bit integers (binary, big endian)" + nl)
-    else:
-        fp.write("data type = 16 bit integers (binary, little endian)" + nl)
-    fp.write("number of frames = " + str(number_frames) + nl)
-
-    # Camera related
-    fp.write("frame size = " + str(c.get("x_pixels") * c.get("y_pixels")) + nl)
-    fp.write("frame dimensions = " + str(c.get("x_pixels")) + " x " + str(c.get("y_pixels")) + nl)
-    fp.write("binning = " + str(c.get("x_bin")) + " x " + str(c.get("y_bin")) + nl)
-
-    # Only save the following for actual cameras.
-    if not c.has("source"):
-        if hasattr(c, "frame_transfer_mode") and c.frame_transfer_mode:
-            fp.write("CCD mode = frame-transfer" + nl)
-        fp.write("horizontal shift speed = " + str(c.get("hsspeed", "NA")) + nl)
-        fp.write("vertical shift speed = " + str(c.get("vsspeed", "NA")) + nl)
-        fp.write("EMCCD Gain = " + str(c.get("emccd_gain", "NA")) + nl)
-        fp.write("Preamp Gain = " + str(c.get("preampgain", "NA")) + nl)
-        fp.write("Exposure Time = " + str(c.get("exposure_value", "NA")) + nl)
-        fp.write("Frames Per Second = " + str(1.0/c.get("cycle_value", "NA")) + nl)
-        fp.write("camera temperature (deg. C) = " + str(c.get("actual_temperature", "NA")) + nl)
-        fp.write("camera head = " + str(c.get("head_model", "NA")) + nl)
-        fp.write("ADChannel = " + str(c.get("adchannel", "NA")) + nl)
-        fp.write("scalemax = " + str(c.get("scalemax")) + nl)
-        fp.write("scalemin = " + str(c.get("scalemin")) + nl)
-
-        fp.write("x_start = " + str(c.get("x_start")) + nl)
-        fp.write("x_end = " + str(c.get("x_end")) + nl)
-        fp.write("y_start = " + str(c.get("y_start")) + nl)
-        fp.write("y_end = " + str(c.get("y_end")) + nl)
-
-        # Additional info
-        stage_pos = p.get("acquisition.stage_position").split(",")
-        fp.write("Stage X = " + stage_pos[0] + nl)
-        fp.write("Stage Y = " + stage_pos[1] + nl)
-        fp.write("Stage Z = " + stage_pos[2] + nl)
-        fp.write("Lock Target = " + str(p.get("acquisition.lock_target")) + nl)
-         
-    fp.write("notes = " + str(p.get("film.notes")) + nl)
-    fp.close()
-
+#def writeInfFile(filename, number_frames, parameters, camera):
+#    c = camera
+#    fp = open(filename + ".inf", "w")
+#    nl = "\n"
+#    p = parameters
+#
+#    # General info
+#    fp.write("information file for" + nl)
+#    fp.write(filename + nl)
+#    fp.write("software version = " + software_version + nl)
+#    fp.write("machine name = " + p.get("setup_name") + nl)
+#    fp.write("parameters file = " + p.get("parameters_file") + nl)
+#    fp.write("shutters file = " + p.get("illumination.shutters", "NA") + nl)
+#    if p.get("film.want_big_endian"):
+#        fp.write("data type = 16 bit integers (binary, big endian)" + nl)
+#    else:
+#        fp.write("data type = 16 bit integers (binary, little endian)" + nl)
+#    fp.write("number of frames = " + str(number_frames) + nl)
+#
+#    # Camera related
+#    fp.write("frame size = " + str(c.get("x_pixels") * c.get("y_pixels")) + nl)
+#    fp.write("frame dimensions = " + str(c.get("x_pixels")) + " x " + str(c.get("y_pixels")) + nl)
+#    fp.write("binning = " + str(c.get("x_bin")) + " x " + str(c.get("y_bin")) + nl)
+#
+#    # Only save the following for actual cameras.
+#    if not c.has("source"):
+#        if hasattr(c, "frame_transfer_mode") and c.frame_transfer_mode:
+#            fp.write("CCD mode = frame-transfer" + nl)
+#        fp.write("horizontal shift speed = " + str(c.get("hsspeed", "NA")) + nl)
+#        fp.write("vertical shift speed = " + str(c.get("vsspeed", "NA")) + nl)
+#        fp.write("EMCCD Gain = " + str(c.get("emccd_gain", "NA")) + nl)
+#        fp.write("Preamp Gain = " + str(c.get("preampgain", "NA")) + nl)
+#        fp.write("Exposure Time = " + str(c.get("exposure_value", "NA")) + nl)
+#        fp.write("Frames Per Second = " + str(1.0/c.get("cycle_value", "NA")) + nl)
+#        fp.write("camera temperature (deg. C) = " + str(c.get("actual_temperature", "NA")) + nl)
+#        fp.write("camera head = " + str(c.get("head_model", "NA")) + nl)
+#        fp.write("ADChannel = " + str(c.get("adchannel", "NA")) + nl)
+#        fp.write("scalemax = " + str(c.get("scalemax")) + nl)
+#        fp.write("scalemin = " + str(c.get("scalemin")) + nl)
+#
+#        fp.write("x_start = " + str(c.get("x_start")) + nl)
+#        fp.write("x_end = " + str(c.get("x_end")) + nl)
+#        fp.write("y_start = " + str(c.get("y_start")) + nl)
+#        fp.write("y_end = " + str(c.get("y_end")) + nl)
+#
+#        # Additional info
+#        stage_pos = p.get("acquisition.stage_position").split(",")
+#        fp.write("Stage X = " + stage_pos[0] + nl)
+#        fp.write("Stage Y = " + stage_pos[1] + nl)
+#        fp.write("Stage Z = " + stage_pos[2] + nl)
+#        fp.write("Lock Target = " + str(p.get("acquisition.lock_target")) + nl)
+#         
+#    fp.write("notes = " + str(p.get("film.notes")) + nl)
+#    fp.close()
+#
 #def writeInfFile(file_class, stage_position, lock_target):
 #        fp = open(file_class.filename + ".inf", "w")
 #        p = file_class.parameters
 #        nl =  "\n"
-
-
 
 class BaseFileWriter(object):
 
@@ -155,6 +153,7 @@ class DaxFile(BaseFileWriter):
         w = str(self.feed_params["x_pixels"])
         h = str(self.feed_params["y_pixels"])
         with open(self.basename + ".inf", "w") as inf_fp:
+            inf_fp.write("binning = 1 x 1\n")
             inf_fp.write("data type = 16 bit integers (binary, little endian)\n")
             inf_fp.write("frame dimensions = " + w + " x " + h + "\n")
             inf_fp.write("number of frames = " + str(self.number_frames) + "\n")
