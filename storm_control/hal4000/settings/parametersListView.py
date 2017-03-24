9c36253054b10b1a062383a485360a782ab0c600
Hazen Babcock <hbabcock@mac.com>
2017-03-24 15:04:32 -0400

    Get user sortable list view working.

--- storm_control/hal4000/settings/parametersListView.py
+++ storm_control/hal4000/settings/parametersListView.py
@@ -8,6 +8,49 @@ Hazen 03/17
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
+def getItemData(q_std_item):
+    return q_std_item.data()
+
+class ParametersItemData(object):
+    """
+    WTF? Apparently Qt clones items when dragging and dropping
+    making it very difficult use customized QStandardItems in
+    a re-orderable list. So instead we store the data in an object
+    and store the object in a QStandardItem. Hackish..
+    """
+    def __init__(self, parameters = None, **kwds):
+        super().__init__(**kwds)
+        self.checked = False
+        self.initialized = False
+        self.parameters = parameters
+
+    def getChecked(self):
+        return self.checked
+
+    def getParameters(self):
+        return self.parameters
+
+    
+class ParametersListViewDelegate(QtWidgets.QStyledItemDelegate):
+    """
+    This lets us draw items that look like radio buttons.
+    """
+    def __init__(self, model = None, **kwds):
+        super().__init__(**kwds)
+        self.model = model
+
+    def paint(self, painter, option, index):
+        note = self.model.itemFromIndex(index)
+        print(type(getItemData(note)))
+        
+        opt = QtWidgets.QStyleOptionButton()
+        opt.rect = option.rect
+        opt.text = note.text()
+        
+        style = option.widget.style()
+        style.drawControl(QtWidgets.QStyle.CE_RadioButton, opt, painter, option.widget)
+        
+        
 class ParametersMVC(QtWidgets.QListView):
     
     def __init__(self, parent = None, **kwds):
@@ -20,16 +63,25 @@ class ParametersMVC(QtWidgets.QListView):
         # This enables the user to re-order the items by dragging them.
         self.setDragDropMode(QtWidgets.QAbstractItemView.InternalMove)
 
+        self.setItemDelegate(ParametersListViewDelegate(model = self.model))
+
         for name in ["setting 1", "setting 2", "setting 3"]:
-            self.model.appendRow(ParametersStandardItem(name))
-            
-
-class ParametersStandardItem(QtGui.QStandardItem):
-    pass
-
+            qitem = QtGui.QStandardItem(name)
+            qitem.setData(ParametersItemData())
+            self.model.appendRow(qitem)
+    
     
 class ParametersStandardItemModel(QtGui.QStandardItemModel):
-    pass
+
+    def flags(self, index):
+        """
+        This blocks overwriting items during drag and drop re-ordering.
+        """
+        if index.isValid(): 
+            return QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsDragEnabled | QtCore.Qt.ItemIsEnabled
+
+        return QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsDragEnabled| \
+            QtCore.Qt.ItemIsDropEnabled | QtCore.Qt.ItemIsEnabled        
 
 #
 # The MIT License
