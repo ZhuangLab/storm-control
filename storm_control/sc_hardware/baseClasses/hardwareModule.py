ed32027dbd2f8ff60333c8a29bfe24d337447974
Hazen Babcock <hbabcock@fas.harvard.edu>
2017-04-18 17:01:51 -0400

    Initial work on adding a buffered functionality.

--- storm_control/sc_hardware/baseClasses/hardwareModule.py
+++ storm_control/sc_hardware/baseClasses/hardwareModule.py
@@ -7,11 +7,22 @@ this module.
 Hazen 04/17
 """
 
+import copy
+
+from PyQt5 import QtCore
+
 import storm_control.hal4000.halLib.halFunctionality as halFunctionality
 import storm_control.hal4000.halLib.halMessage as halMessage
 import storm_control.hal4000.halLib.halModule as halModule
 
 
+def getThreadPool(self):
+    """
+    Return the applications threadpool instance. This is stored in halModule.
+    """
+    return halModule.threadpool
+
+    
 class HardwareFunctionality(halFunctionality.HalFunctionality):
     """
     These are requested using the 'get functionality' message. The
@@ -20,6 +31,43 @@ class HardwareFunctionality(halFunctionality.HalFunctionality):
     """
     pass
 
+
+class BufferedFunctionality(HardwareFunctionality):
+    """
+    This is used to communicate with less responsive hardware. There
+    may be several of these per device, self.device_mutex is used
+    to coordinate.
+    """
+    done = QtCore.pyqtSignal()
+    
+    def __init__(self, device_mutex = QtCore.QMutex(), **kwds):
+        super().__init__(**kwds)
+        self.current_request = None
+        self.device_mutex = device_mutex
+
+        self.done.connect(self.handleDone)
+
+    def handleDone(self):
+        if self.current_request is not None:
+            request = copy.copy(self.current_request)
+            self.current_request = None
+            getThreadPool().start(self.run(request))
+
+    def handleRequest(self, request):
+        if self.current_request is None:
+            getThreadPool().start(self.run(copy.copy(request)))
+        self.current_request = request
+
+    def processRequest(self, request):
+        pass
+    
+    def run(self, request):
+        self.device_mutex.lock()
+        self.processRequest(request)
+        self.device_mutex.unlock()
+        self.done.emit()
+
+        
 
 class HardwareModule(halModule.HalModule):
 
