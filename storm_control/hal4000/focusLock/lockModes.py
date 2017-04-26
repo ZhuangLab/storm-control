fedca7cc6a63e7185afb883a0a56e4eb261f1a8c
Hazen Babcock <hbabcock@mac.com>
2017-04-26 13:12:20 -0400

    Restore optimal and calibration modes.

--- storm_control/hal4000/focusLock/lockModes.py
+++ storm_control/hal4000/focusLock/lockModes.py
@@ -30,8 +30,10 @@ class LockModeException(halExceptions.HalException):
 # their name.
 #
 # FIXME: Was this actually a good idea? Getting the inheritance
-#        to work correctly is messy. Maybe these should just
-#        have been different class of objects?
+#        to work correctly is messy. It is also a bit difficult
+#        to follow what exactly is going to happen when a
+#        a particular method like startLock() is called. Maybe
+#        these should just have been different class of objects?
 #
 class FindSumMixin(object):
     """
@@ -186,9 +188,6 @@ class LockedMixin(object):
             self.lm_counter += 1
             if (self.lm_counter == self.lm_buffer_length):
                 self.lm_counter = 0
-
-    def initializeLMBuffer(self):
-        self.lm_buffer = numpy.zeros(self.lm_buffer_length, dtype = numpy.uint8)
             
     def newParameters(self, parameters):
         if hasattr(super(), "newParameters"):
@@ -197,11 +196,16 @@ class LockedMixin(object):
         self.lm_buffer_length = parameters.get("buffer_length")
         self.lm_min_sum = parameters.get("minimum_sum")
         self.lm_offset_threshold = parameters.get("offset_threshold")
-                
+
+    def startLock(self):
+        print(">sl")
+        self.lm_counter = 0
+        self.lm_buffer = numpy.zeros(self.lm_buffer_length, dtype = numpy.uint8)
+        self.behavior = "locked"
+
     def startLockBehavior(self, behavior_name, behavior_params):
         if (behavior_name == self.lm_mode_name):
-            self.lm_counter = 0
-
+            
             if "buffer_length" in behavior_params:
                 self.lm_buffer_length = behavior_params["buffer_length"]
             else:
@@ -228,7 +232,7 @@ class LockedMixin(object):
             if "z_start" in behavior_params:
                 self.z_stage_functionality.goAbsolute(behavior_params["z_start"])
 
-            self.initializeLMBuffer()
+            self.startLock()
     
 
 class LockMode(QtCore.QObject):
@@ -244,7 +248,11 @@ class LockMode(QtCore.QObject):
     if the zstage is slow it could get overwhelmed by move requests.
 
     The modes share a single parameter object. The parameters specific
-    to a particular mode are stored under the modes 'name' attribute.
+    to a particular mode are stored under a mode specific attribute.
+
+    To avoid name clashes as there are a lot of attributes (too many?),
+    sub-class attribute names are all prefixed with a sub-class
+    specific string.
     """
     # This signal is emitted when a mode finishes,
     # with True/False for success or failure.
@@ -307,7 +315,6 @@ class LockMode(QtCore.QObject):
             super().newParameters(parameters)
 
     def setLockStatus(self, status):
-        print(">sll", status)
         self.good_lock = status
         self.goodLock.emit(status)
         
@@ -325,7 +332,9 @@ class LockMode(QtCore.QObject):
         pass
 
     def startLock(self):
-        pass
+        self.setLockStatus(False)
+        if hasattr(super(), "startLock"):
+            super().startLock()
         
     def startLockBehavior(self, behavior_name, behavior_params):
         """
@@ -355,10 +364,10 @@ class JumpLockMode(LockMode, FindSumMixin, LockedMixin):
     def __init__(self, **kwds):
         super().__init__(**kwds)
 
-        self.relock_timer = QtCore.QTimer(self)
-        self.relock_timer.setInterval(200)
-        self.relock_timer.setSingleShot(True)
-        self.relock_timer.timeout.connect(self.handleRelockTimer)
+        self.jlm_relock_timer = QtCore.QTimer(self)
+        self.jlm_relock_timer.setInterval(200)
+        self.jlm_relock_timer.setSingleShot(True)
+        self.jlm_relock_timer.timeout.connect(self.handleRelockTimer)
 
     def handleJump(self, jumpsize):
         """
@@ -367,9 +376,9 @@ class JumpLockMode(LockMode, FindSumMixin, LockedMixin):
         """
         if (self.behavior == "locked"):
             self.behavior = "none"
-            self.relock_timer.start()
+            self.jlm_relock_timer.start()
         self.z_stage_functionality.goRelative(jumpsize)
-        
+
     def handleRelockTimer(self):
         """
         Restarts the focus lock when the relock timer fires.
@@ -378,7 +387,7 @@ class JumpLockMode(LockMode, FindSumMixin, LockedMixin):
 
 
 #
-# These are in the order that they usually appear in the combo box.
+# These are in the order that they (usually) appear in the combo box.
 #
 class NoLockMode(LockMode):
     """
@@ -409,11 +418,12 @@ class AutoLockMode(JumpLockMode):
     def startFilm(self):
         self.startLock()
 
-    def startLock(self):
-        self.setLockStatus(False)
-        self.initializeLMBuffer()
-        self.setLockTarget(self.qpd_state["offset"])
-        self.behavior = "locked"
+    def startLock(self, target = None):
+        super().startLock()
+        if target is None:
+            self.setLockTarget(self.qpd_state["offset"])
+        else:
+            self.setLockTarget(target)
 
     def stopFilm(self):
         self.stopLock()
@@ -427,7 +437,7 @@ class AlwaysOnLockMode(AutoLockMode):
     """
     def __init__(self, **kwds):
         super().__init__(**kwds)
-        self.film_on = False
+        self.aolm_film_on = False
         self.name = "Always On"
 
     def shouldEnableLockButton(self):
@@ -435,14 +445,258 @@ class AlwaysOnLockMode(AutoLockMode):
 
     def startFilm(self):
         if not self.amLocked():
-            self.film_on = True
+            self.aolm_film_on = True
             self.startLock()
 
     def stopFilm(self):
-        if self.film_on:
-            self.film_on = False
+        if self.aolm_film_on:
+            self.aolm_film_on = False
             self.stopLock()
-    
+
+
+class OptimalLockMode(AlwaysOnLockMode):
+    """
+    At the start of filming the stage is moved in a triangle wave. 
+    First it goes up to bracket_step, then down to -bracket_step 
+    and then finally back to zero. At each point along the way the 
+    focus quality & offset are recorded. When the stage returns to 
+    zero, the data is fit with a gaussian and the lock target is 
+    set to the offset corresponding to the center of the gaussian.
+    """
+    def __init__(self, parameters = None, **kwds):
+        kwds[parameters] = parameters
+        super().__init__(**kwds)
+        self.name = "Optimal"
+        self.olm_bracket_step = None
+        self.olm_counter = 0
+        self.olm_fvalues = None
+        self.olm_mode = "none"
+        self.olm_pname = "optimal_mode"
+        self.olm_quality_threshold = 0
+        self.olm_relative_z = None
+        self.olm_scan_hold = None
+        self.olm_scan_step = None
+        self.olm_scan_state = "na"
+        self.olm_zvalues = None
+
+        # Add optimal lock specific parameters.
+        p = self.parameters.addSubSection(self.olm_pname)
+        p.add(params.ParameterRangeFloat(description = "Distance +- z in nanometers",
+                                         name = "bracket_step",
+                                         value = 1000.0,
+                                         minimum = 10.0,
+                                         maximum = 10000.0))
+        p.add(params.ParameterRangeFloat(description = "Minimum 'quality' signal",
+                                         name = "quality_threshold",
+                                         value = 0.0,
+                                         minimum = 0.0,
+                                         maximum = 1000.0))        
+        p.add(params.ParameterRangeFloat(description = "Step size in z in nanometers",
+                                         name = "scan_step",
+                                         value = 100.0,
+                                         minimum = 10.0,
+                                         maximum = 1000.0))
+        p.add(params.ParameterRangeInt(description = "Frames to pause between steps",
+                                       name = "scan_hold",
+                                       value = 10,
+                                       minimum = 1,
+                                       maximum = 100))
+        
+    def initializeScan(self):
+        """
+        Configures all the variables that will be used during 
+        the scan to find the optimal lock target.
+        """
+        self.olm_mode = "optimizing"
+        self.olm_relative_z = 0.0
+        self.olm_scan_state = "scan up"
+        self.olm_counter = 0
+        size_guess = round(self.scan_hold * (self.bracket_step / self.scan_step) * 6)
+        self.olm_fvalues = numpy.zeros(size_guess)
+        self.olm_zvalues = numpy.zeros(size_guess)
+
+    def newFrame(self, frame):
+        """
+        Handles a new frame from the camera. If the mode is optimizing this calculates
+        the focus quality of the frame and moves the piezo to its next position.
+        """
+        if (self.olm_mode == "optimizing"):
+            quality = focusQuality.imageGradient(frame)
+            if (quality > self.olm_quality_threshold):
+                self.olm_zvalues[self.counter] = self.qpd_state["offset"]
+                self.olm_fvalues[self.counter] = quality
+                self.olm_counter += 1
+
+                if ((self.old_counter % self.olm_scan_hold) == 0):
+
+                    # Scan up
+                    if (self.olm_scan_state == "scan up"):
+                        if (self.olm_relative_z >= self.olm_bracket_step):
+                            self.olm_scan_state = "scan down"
+                        else:
+                            self.olm_relative_z += self.olm_scan_step
+                            self.z_stage_functionality.goRelative(self.olm_scan_step)
+                            
+                    # Scan back down                            
+                    elif (self.olm_scan_state == "scan down"): 
+                        if (self.olm_relative_z <= -self.olm_bracket_step):
+                            self.scan_state = "zero"
+                        else:
+                            self.olm_relative_z -= self.olm_scan_step
+                            self.z_stage_functionality.goRelative(-self.olm_scan_step)
+
+                    # Scan back to zero                            
+                    else: 
+                        if (self.olm_relative_z >= 0.0):
+                            n = self.olm_counter - 1
+
+                            # Fit offset data to a 1D gaussian (lorentzian would be better?)
+                            zvalues = self.olm_zvalues[0:n]
+                            fvalues = self.olm_fvalues[0:n]
+                            fitfunc = lambda p, x: p[0] + p[1] * numpy.exp(- (x - p[2]) * (x - p[2]) * p[3])
+                            errfunc = lambda p: fitfunc(p, zvalues) - fvalues
+                            p0 = [numpy.min(fvalues),
+                                  numpy.max(fvalues) - numpy.min(fvalues),
+                                  zvalues[numpy.argmax(fvalues)],
+                                  9.0] # empirically determined width parameter
+                            p1, success = scipy.optimize.leastsq(errfunc, p0[:])
+                            if (success == 1):
+                                optimum = p1[2]
+                            else:
+                                print("> fit for optimal lock failed.")
+                                # hope that this is close enough
+                                optimum = zvalues[numpy.argmax(fvalues)]
+
+                            print("> optimal Target:", optimum)
+                            self.olm_mode = "none"
+                            self.startLock(target = optimum)
+                        else:
+                            self.olm_relative_z += self.olm_scan_step
+                            self.z_stage_functionality.goRelative(self.olm_scan_step)
+
+    def newParameters(self, parameters):
+        if hasattr(super(), "newParameters"):
+            super().newParameters(parameters)
+        p = parameters.get(self.olm_pname)
+        self.olm_bracket_step = 0.001 * p.get("bracket_step")
+        self.olm_quality_threshold = p.get("quality_threshold")
+        self.olm_scan_step = 0.001 * p.get("scan_step")
+        self.olm_scan_hold = p.get("scan_hold")
+
+    def startFilm(self):
+        self.behavior = "none"
+        self.initScan()
+        
+
+class CalibrationLockMode(JumpLockMode):
+    """
+    No lock, the stage is driven through a pre-determined set of 
+    z positions for calibration purposes during filming.
+    """
+    def __init__(self, parameters = None, **kwds):
+        kwds[parameters] = parameters    
+        super().__init__(**kwds)
+        self.clm_counter = 0
+        self.clm_max_zvals = 0
+        self.clm_pname = "calibrate"
+        self.clm_zvals = []
+        self.name = "Calibrate"
+
+        # Add calibration specific parameters.
+        p = self.parameters.addSubSection(self.clm_pname)
+        p.add(params.ParameterRangeInt(description = "Frames to pause between steps.",
+                                       name = "frames_to_pause",
+                                       value = 2,
+                                       minimum = 1,
+                                       maximum = 100))        
+        p.add(params.ParameterRangeInt(description = "Frames before to pause at start.",
+                                       name = "deadtime",
+                                       value = 20,
+                                       minimum = 1,
+                                       maximum = 100))
+        p.add(params.ParameterRangeFloat(description = "Distance +- z to move in nanometers.",
+                                         name = "range",
+                                         value = 600,
+                                         minimum = 100,
+                                         maximum = 5000))
+        p.add(params.ParameterRangeFloat(description = "Step size in z in nanometers.",
+                                         name = "cal_step_size",
+                                         value = 10,
+                                         minimum = 1,
+                                         maximum = 100))
+        
+    def calibrationSetup(self, z_center, deadtime, zrange, step_size, frames_to_pause):
+        """
+        Configure the variables that will be used to execute the z scan.
+        """
+        # FIXME: Are these checks a good idea?
+        if False:
+            if (deadtime <= 0):
+                raise LockModeException("Deadtime is too small " + str(deadtime))
+            if (zrange < 10):
+                raise LockModeException("Range is too small " + str(zrange))
+            if (zrange > 1000):
+                raise LockModeException("Range is too large " + str(zrange))
+            if (step_size <= 0.0):
+                raise LockModeException("Negative / zero step size " + str(step_size))
+            if (step_size > 100.0):
+                raise LockModeException("Step size is to large " + str(step_size))
+            if (frames_to_pause <= 0):
+                raise LockModeException("Frames to pause is too smale " + str(frames_to_pause))
+
+        def addZval(z_val):
+            self.clm_zvals.append(z_val)
+            self.clm_max_zvals += 1
+
+        self.clm_zvals = []
+        self.clm_max_zvals = 0
+        
+        # Convert to um.
+        zrange = 0.001 * zrange
+        step_size = 0.001 * step_size
+
+        # Initial hold.
+        for i in range(deadtime-1):
+            addZval(z_center)
+
+        # Staircase scan.
+        addZval(-zrange)
+        z = z_center - zrange
+        stop = z_center + zrange - 0.5 * step_size
+        while (z < stop):
+            for i in range(frames_to_pause-1):
+                addZval(0.0)
+            addZval(step_size)
+            z += step_size
+
+        addZval(-zrange)
+
+        # Final hold.
+        for i in range(deadtime-1):
+            addZval(z_center)
+
+    def newFrame(self, frame):
+        """
+        Handles a new frame from the camera. This moves to a new z position
+        if the scan has not been completed.
+        """
+        if (self.clm_counter < self.clm_max_zvals):
+            self.z_stage_functionality.goRelative(self.zvals[self.counter])
+            self.clm_counter += 1
+
+    def newParameters(self, parameters):
+        if hasattr(super(), "newParameters"):
+            super().newParameters(parameters)
+        p = parameters.get(self.clm_pname)
+        self.calibrationSetup(0.0, 
+                              p.get("deadtime"), 
+                              p.get("range"), 
+                              p.get("step_size"), 
+                              p.get("frames_to_pause"))
+
+    def startFilm(self):
+        self.clm_counter = 0
+
 
 #
 # The MIT License
