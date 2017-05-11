ba2123749457b8ee5c81647a890f7a6251f0e62d
Hazen Babcock <hbabcock@mac.com>
2017-05-11 17:28:55 -0400

    Initial work on handling parameter file requests and verifying that we won't overwrite (unless asked).

--- storm_control/hal4000/tcpControl/tcpControl.py
+++ storm_control/hal4000/tcpControl/tcpControl.py
@@ -24,8 +24,11 @@ class Controller(QtCore.QObject):
     
     def __init__(self, server = None, verbose = True, **kwds):
         super().__init__(**kwds)
-        self.film_message = None
+        self.directory = None
         self.server = server
+        self.tcp_message = None
+        self.test_directory = None
+        self.test_parameters = None
         self.verbose = verbose
 
         self.server.comGotConnection.connect(self.handleNewConnection)
@@ -39,12 +42,30 @@ class Controller(QtCore.QObject):
         """
         This method will get called when film.film sends a 'film lockout' message with
         'locked out' False, which is the signal for the end of a film.
+
+        Note: The 'None' check is because we could also end up here if the user 
+              requested a film via the GUI (i.e. they clicked the record button).
         """
-        if self.film_message is not None:
-            self.server.sendMessage(self.film_message)
-        self.film_message = None
+        if self.tcp_message is not None:
+            self.server.sendMessage(self.tcp_message)
+        self.tcp_message = None
+
+    def getParametersComplete(self, parameters):
+        """
+        This method is called when settings.settings responds to a request for 
+        parameters.
+        """
+        # FIXME: Need to check that the parameters are initialized.
+        if parameters is None:
+            self.tcp_message.setError(True, "Requested parameters not found")
+        else:
+            self.test_parameters = parameters
+        self.server.sendMessage(self.tcp_message)
+        self.tcp_message = None
         
     def handleLostConnection(self):
+        self.test_directory = self.directory
+        self.test_parameters = None
         self.controlMessage.emit(halMessage.HalMessage(m_type = "configuration",
                                                        data = {"properties" : {"connected" : False}}))
 
@@ -63,6 +84,7 @@ class Controller(QtCore.QObject):
             if not os.path.isdir(directory):
                 tcp_message.setError(True, directory + " is an invalid directory")
             else:
+                self.test_directory = directory
                 if not tcp_message.isTest():
                     self.controlMessage.emit(halMessage.HalMessage(m_type = "change directory",
                                                                    data = {"directory" : directory},
@@ -70,27 +92,59 @@ class Controller(QtCore.QObject):
                     return
             self.server.sendMessage(tcp_message)
 
+        elif tcp_message.isType("Set Parameters"):
+
+            # Fail if we are currently handling a different message.
+            assert self.tcp_message is None
+            
+            if tcp_message.isTest():
+                self.controlMessage.emit(halMessage.HalMessage(m_type = "get parameters",
+                                                               data = {"index or name" : tcp_message.getData("parameters")}))
+            else:
+                self.controlMessage.emit(halMessage.HalMessage(m_type = "set parameters",
+                                                               data = {"index or name" : tcp_message.getData("parameters")}))
+                    
         elif tcp_message.isType("Take Movie"):
 
             # Check that movie length is valid.
             if (tcp_message.getData("length") is None) or (tcp_message.getData("length") < 1):
-                tcp_message.setError(True, str(message.getData("length")) + " is an invalid movie length")
+                tcp_message.setError(True, str(message.getData("length")) + " is an invalid movie length.")
                 self.server.sendMessage(tcp_message)
                 return
 
-            # Some messy logic here to check if we will over-write an existing film?
-            
+            # Some messy logic here to check if we will over-write a existing films? For now, just
+            # verify that the movie.xml file does not exist.
+            if not tcp_message.getData("overwrite"):
+                directory = tcp_message.getData("directory"):
+                if directory is None:
+                    directory = self.test_directory
+
+                filename = os.path.join(directory, tcp_message.getData("name")) + ".xml"
+                if os.path.exists(filename):
+                    tcp_message.setError(True, "The movie file '" + filename + "' already exists.")
+                    self.server.sendMessage(tcp_message)
+                    return
+
+            # Fail if we are currently handling a different message.
+            assert self.tcp_message is None
+
             if tcp_message.isTest():
                 # More messy logic here to return film size, time, etc.. here.
                 pass
             else:
+                # Check if we need to change parameters first.
+                if tcp_message.getData("parameters") is not None:
+                    pass
+                
+                # Take movie.
                 film_request = filmRequest.FilmRequest(basename = tcp_message.getData("name"),
                                                        directory = tcp_message.getData("directory"),
                                                        frames = tcp_message.getData("length"),
+                                                       overwrite = tcp_message.getData("overwrite"),
                                                        tcp_request = True)
                 self.controlMessage.emit(halMessage.HalMessage(m_type = "start film request",
                                                                data = {"request" : film_request}))
-                self.film_message = tcp_message
+                self.tcp_message = tcp_message
 
         # Everything else is (in theory) handled by other modules.
         else:
@@ -107,6 +161,21 @@ class Controller(QtCore.QObject):
             tcp_message.setError(True, "This message was not handled.")
         self.server.sendMessage(tcp_message)
 
+    def setDirectory(self, directory):
+        self.directory = directory
+        
+    def setParametersComplete(self, parameters):
+        """
+        This method will get called when settings.settings sends a 'updated parameters' which 
+        is the signal that the parameters have been updated.
+
+        Note: The 'None' check is because we could also end up here if the user 
+              requested a film via the GUI (i.e. they clicked the record button).
+        """
+        if self.tcp_message is not None:
+            self.server.sendMessage(self.tcp_message)
+        self.tcp_message = None
+        
 
 class TCPControl(halModule.HalModule):
     """
@@ -139,7 +208,11 @@ class TCPControl(halModule.HalModule):
         self.sendMessage(message)
 
     def handleResponses(self, message):
-        if message.isType("tcp message"):
+        if message.isType("get parameters"):
+            assert (len(message.getResponses()) == 1)
+            self.control.getParametersComplete(message.getResponses()[0])
+
+        elif message.isType("tcp message"):
             #
             # FIXME: We might want to check for 'True' responses? Not sure
             #        why we'd ever respond with 'False' however.
@@ -149,10 +222,15 @@ class TCPControl(halModule.HalModule):
 
     def processMessage(self, message):
 
-        if message.isType("film lockout"):
+        if message.isType("change directory"):
+            self.control.setDirectory(message.getData()["directory"])
+            
+        elif message.isType("film lockout"):
             if not message.getData()["locked out"]:
                 self.control.filmComplete()
-        
+
+        elif message.isType("updated parameters"):
+            self.control.setParametersComplete(message.getData()["parameters"])
 
 #
 # The MIT License
