3601a5e399837d0e1e3451294729e1a5f7c9add7
Hazen Babcock <hbabcock@mac.com>
2017-05-13 15:28:16 -0400

    Finish updating to work in HAL2 (untested).

--- storm_control/sc_hardware/andor/w1SpinDiskModule.py
+++ storm_control/sc_hardware/andor/w1SpinDiskModule.py
@@ -9,33 +9,23 @@ Hazen 5/17
 import storm_control.hal4000.halLib.halMessage as halMessage
 
 import storm_control.sc_hardware.baseClasses.hardwareModule as hardwareModule
-import storm_control.sc_hardware.serial.RS232 as RS232
+import storm_control.sc_hardware.andor.w1SpinningDisk as w1SpinningDisk
 
 import storm_control.sc_library.halExceptions as halExceptions
 import storm_control.sc_library.parameters as params
 
 
-class W1Exception(halExceptions.HardwareException):
-    """
-    Spinning disk exception.
-    """
-    def __init__(self, message):
-        halExceptions.HardwareException.__init__(self, message)
-
-
-class W1Functionality(hardwareModule.BufferedFunctionality):
+class W1Control(object):
     """
     Control of the W1 spinning disk. 
-
-    This is only a functionality for convenience. It is not 
-    shared with other HAL modules.
     """
     def __init__(self, w1 = None, configuration = None, **kwds):
         super().__init__(**kwds)
         self.w1 = w1
 
         # Query W1 for it's maximum speed.
-        max_speed = self.w1.commWithResp("MS_MAX,?")
+        max_speed = self.w1.commandResponse("MS_MAX,?")
+        assert max_speed is not None
 
         # Create dictionaries for the configuration of the
         # filter wheels and two dichroic mirror sets.
@@ -63,158 +53,166 @@ class W1Functionality(hardwareModule.BufferedFunctionality):
         for pos, camera_dichroic in enumerate(camera_dichroic_names):
             self.camera_dichroic_config[camera_dichroic] = pos + 1
 
-        # Define error codes
-        self.error_codes = {"30005": "Command name error",
-                            "30006": "Command argument number error",
-                            "30007": "Command argument value error",
-                            "30141": "Command argument value error",
-                            "30012": "Interlock alarm is on",
-                            "30133": "Interlock alarm is on",
-                            "30014": "Electricity alarm is on",
-                            "30015": "Shutter alarm is on",
-                            "30016": "Actuator alarm is on",
-                            "30017": "Disk alarm is on",
-                            "30018": "Data error alarm is on",
-                            "30019": "Other alarm is on",
-                            "30021": "Designated system is not defined",
-                            "30022": "Designated system does not exist",
-                            "30023": "Designated system is not detected",
-                            "30031": "Waiting for initialization to complete",
-                            "30032": "Under maintenance mode",
-                            "30201": "External SYNC signal is under use",
-                            "30204": "Disk rotation stopped",
-                            "30301": "Shutter error",
-                            "30302": "Shutter unopenable error",
-                            "1": "Unknown serial communication error"}
-
         # Create parameters
         self.parameters = params.StormXMLObject()
 
-        sd_params.add("bright_field_bypass", params.ParameterSetBoolean("Bypass spinning disk for brightfield mode?",
-                                                                        "bright_field_bypass", False))
+        self.parameters.add(params.ParameterSetBoolean(description = "Bypass spinning disk for brightfield mode?",
+                                                       name = "bright_field_bypass",
+                                                       value = False))
 
-        sd_params.add("spin_disk", params.ParameterSetBoolean("Spin the disk?",
-                                                              "spin_disk", True))
-
+        self.parameters.add(params.ParameterSetBoolean(description = "Spin the disk?",
+                                                       name = "spin_disk",
+                                                       value = True))
+        
         # Disk properties
-        sd_params.add("disk", params.ParameterSetString("Disk pinhole size",
-                                                        "disk",
-                                                        "50-micron pinholes",
-                                                        ["50-micron pinholes", "25-micron pinholes"]))
+        self.parameters.add(params.ParameterSetString(description = "Disk pinhole size",
+                                                      name = "disk",
+                                                      value = "50-micron pinholes",
+                                                      allowed = ["50-micron pinholes", "25-micron pinholes"]))
 
-        max_speed = self.getMaxSpeed()
-        sd_params.add("disk_speed", params.ParameterRangeInt("Disk speed (RPM)",
-                                                             "disk_speed",
-                                                             max_speed, 1, max_speed))
+        self.parameters.add(params.ParameterRangeInt(description = "Disk speed (RPM)",
+                                                     name = "disk_speed",
+                                                     value = max_speed,
+                                                     min_value = 1,
+                                                     max_value = max_speed))
 
         # Dichroic mirror position
         values = sorted(self.dichroic_mirror_config.keys())
-        sd_params.add("dichroic_mirror", params.ParameterSetString("Dichroic mirror position",
-                                                                   "dichroic_mirror",
-                                                                   values[0],
-                                                                   values))
+        self.parameters.add(params.ParameterSetString(description = "Dichroic mirror position",
+                                                      name = "dichroic_mirror",
+                                                      value = values[0],
+                                                      allowed = values))
 
         # Filter wheel positions
         values = sorted(self.filter_wheel_1_config.keys())
-        sd_params.add("filter_wheel_pos1", params.ParameterSetString("Camera 1 Filter Wheel Position (1-10)",
-                                                                    "filter_wheel_pos1",
-                                                                    values[0],
-                                                                    values))
+        self.parameters.add(params.ParameterSetString(description = "Camera 1 Filter Wheel Position (1-10)",
+                                                      name = "filter_wheel_pos1",
+                                                      value = values[0],
+                                                      allowed = values))
 
         values = sorted(self.filter_wheel_2_config.keys())
-        sd_params.add("filter_wheel_pos2", params.ParameterSetString("Camera 2 Filter Wheel Position (1-10)",
-                                                                    "filter_wheel_pos2",
-                                                                    values[0],
-                                                                    values))
+        self.parameters.add(params.ParameterSetString(description = "Camera 2 Filter Wheel Position (1-10)",
+                                                      name = "filter_wheel_pos2",
+                                                      value = values[0],
+                                                      values = values))
 
         # Camera dichroic positions
         values = sorted(self.camera_dichroic_config.keys())
-        sd_params.add("camera_dichroic_mirror", params.ParameterSetString("Camera dichroic mirror position (1-3)",
-                                                                          "camera_dichroic_mirror",
-                                                                          values[0],
-                                                                          values))
+        self.parameters.add(params.ParameterSetString(description = "Camera dichroic mirror position (1-3)",
+                                                      name = "camera_dichroic_mirror",
+                                                      value = values[0],
+                                                      values))
 
         # Aperature settings
-        sd_params.add("aperture", params.ParameterRangeInt("Aperture value (1-10; small to large)",
-                                                            "aperture",
-                                                            10,1,10))
+        self.parameters.add(params.ParameterRangeInt(description = "Aperture value (1-10; small to large)",
+                                                     name = "aperture",
+                                                     value = 10,
+                                                     min_value = 1,
+                                                     max_value = 10))
 
-    # newParameters
-    #
-    # Update the spinning disk parameters (if different from current configuration)
-    #
-    # @param parameters A parameters object
-    def newParameters(self, parameters):
-        p = parameters.get("spinning_disk")
+        self.newParameters(self.parameters, initialization = True)
 
-        # Update all parameters of the spinning disk, checking to see if parameters need updated
-        for key in p.getAttrs():
-            if not (key in self.params.getAttrs()) or not (self.params.get(key) == p.get(key)):
-                if key == "bright_field_bypass":
-                    if p.get("bright_field_bypass"):
-                        self.serial_thread.sendCommand("BF_ON\r")
-                        sleep(1)
-                    else:
-                        self.serial_thread.sendCommand("BF_OFF\r")
-                        sleep(1)
-                elif key == "spin_disk":
-                    if p.get("spin_disk"):
-                        self.serial_thread.sendCommand("MS_RUN\r")
-                        sleep(3)
-                    else:
-                        self.serial_thread.sendCommand("MS_STOP\r")
-                        sleep(1)
-                elif key == "disk":
-                    if p.get("disk") == "50-micron pinholes":
-                        self.serial_thread.sendCommand("DC_SLCT,1\r")
-                        sleep(3)
-                    elif p.get("disk") == "25-micron pinholes":
-                        self.serial_thread.sendCommand("DC_SLCT,2\r")
-                        sleep(3)
-                elif key == "disk_speed":
-                    self.serial_thread.sendCommand("MS,"+str(p.get("disk_speed"))+"\r")
-                    sleep(1)
-                elif key == "dichroic_mirror":
-                    dichroic_num = self.dichroic_mirror_config[p.get("dichroic_mirror")]
-                    self.serial_thread.sendCommand("DMM_POS,1,"+str(dichroic_num)+"\r")
-                    sleep(1)
-                elif key == "filter_wheel_pos1" or key == "filter_wheel_pos2":
-                    filter1_num = self.filter_wheel_1_config[p.get("filter_wheel_pos1")]
-                    filter2_num = self.filter_wheel_2_config[p.get("filter_wheel_pos2")]
-                    self.serial_thread.sendCommand("FW_POS,0," + str(filter1_num) + "," + str(filter2_num) + "\r")
-                    sleep(0.1)
-                elif key == "camera_dichroic_mirror":
-                    camera_dichroic_num = self.camera_dichroic_config[p.get("camera_dichroic_mirror")]
-                    self.serial_thread.sendCommand("PT_POS,1," + str(camera_dichroic_num) + "\r")
-                    sleep(1)
-                elif key == "aperture":
-                    self.serial_thread.sendCommand("AP_WIDTH,1,"+str(p.get("aperture"))+"\r")
-                    sleep(0.5)
+    def getParameters(self):
+        return self.parameters
+    
+    def newParameters(self, parameters, initialization = False):
+
+        if initialization:
+            changed_p_names = parameters.getAttrs()
+        else:
+            changed_p_names = params.difference(parameters, self.parameters)
+
+        p = parameters
+        for pname in changed_p_names:
+
+            # Update our current parameters.
+            self.parameters.setv(pname, p.get(pname))
+
+            # Configure the W1.
+            if (pname == "bright_field_bypass"):
+                if p.get("bright_field_bypass"):
+                    self.w1.commandResponse("BF_ON", 1)
                 else:
-                    print str(key) + " is not a valid parameter for the W1"
+                    self.w1.commandResponse("BF_OFF", 1)
 
-        # Make deep copy of the passed parameters so that the spinning disk remembers its current configuration
-        self.params = copy.deepcopy(p)   
+            elif (pname == "spin_disk"):
+                if p.get("spin_disk"):
+                    self.w1.commandResponse("MS_RUN", 3)
+                else:
+                    self.w1.commandResponse("MS_STOP", 1)
+                    
+            elif (pname == "disk"):
+                if (p.get("disk") == "50-micron pinholes"):
+                    self.w1.commandResponse("DC_SLCT,1", 3)
+                elif (p.get("disk") == "25-micron pinholes"):
+                    self.w1.commandResponse("DC_SLCT,2", 3)
+
+            elif (pname == "disk_speed"):
+                self.w1.commandResponse("MS,"+str(p.get("disk_speed")), 1)
+
+            elif (pname == "dichroic_mirror"):
+                dichroic_num = self.dichroic_mirror_config[p.get("dichroic_mirror")]
+                self.w1.commandResponse("DMM_POS,1,"+str(dichroic_num), 1)
+
+            elif (pname == "filter_wheel_pos1") or (pname == "filter_wheel_pos2"):
+                filter1_num = self.filter_wheel_1_config[p.get("filter_wheel_pos1")]
+                filter2_num = self.filter_wheel_2_config[p.get("filter_wheel_pos2")]
+                self.w1.commandResponse("FW_POS,0," + str(filter1_num) + "," + str(filter2_num), 0.1)
+
+            elif (pname == "camera_dichroic_mirror"):
+                camera_dichroic_num = self.camera_dichroic_config[p.get("camera_dichroic_mirror")]
+                self.w1.commandResponse("PT_POS,1," + str(camera_dichroic_num), 1)
+
+            elif (pname == "aperture"):
+                self.w1.commandResponse("AP_WIDTH,1,"+str(p.get("aperture")), 0.5)
+
+            else:
+                print(">> Warning", str(pname), " is not a valid parameter for the W1")
 
 
 class W1SpinDiskModule(hardwareModule.HardwareModule):
 
     def __init__(self, module_params = None, qt_settings = None, **kwds):
         super().__init__(**kwds)
-        self.w1_fn = None
+        self.control = None
+        self.w1 = None
 
         configuration = module_params.get("configuration")
-        self.w1_tty = RS232.RS232(baudrate = configuration.get("baudrate"),
-                                  port = configuration.get("port"))
-        if self.w1_tty:
-            self.w1_fn = W1Functionality(w1 = self.w1_tty,
-                                         configuration = configuration)
+        self.w1 = w1SpinningDisk.W1SpinningDisk(baudrate = configuration.get("baudrate"),
+                                                port = configuration.get("port"))
+        if self.w1.getStatus():
+            self.control = W1Control(w1 = self.w1,
+                                     configuration = configuration)
 
     def cleanUp(self, qt_settings):
-        if self.w1_fn is not None:
-            self.w1_fn.wait()
-            self.w1_tty.shutDown()
-                        
+        if self.control is not None:
+            self.w1.shutDown()
+
+    def processMessage(self, message):
+
+        if self.control is None:
+            return
+
+        if message.isType("configure1"):
+            self.sendMessage(halMessage.HalMessage(m_type = "initial parameters",
+                                                   data = {"parameters" : self.control.getParameters}))
+
+        #
+        # FIXME? Maybe we want do this at 'update parameters' as we don't
+        #        do any error checking.
+        #
+        elif message.isType("new parameters"):
+            hardwareModule.runHardwareTask(self,
+                                           message,
+                                           lambda : self.updateParameters(message))
+
+    def updateParameters(self, message):
+        message.addResponse(halMessage.HalMessageResponse(source = self.module_name,
+                                                          data = {"old parameters" : self.control.getParameters().copy()}))
+        p = message.getData()["parameters"].get(self.module_name)
+        self.control.newParameters(p)
+        message.addResponse(halMessage.HalMessageResponse(source = self.module_name,
+                                                          data = {"new parameters" : self.control.getParameters()}))
         
 #
 # The MIT License
