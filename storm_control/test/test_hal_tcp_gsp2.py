7c8f9da006249ed974c9eed024bf15a656da2d71
Hazen Babcock <hbabcock@mac.com>
2017-05-19 11:24:16 -0400

    Change files names to match convention that the names are an abbreviated form of the message.

--- storm_control/test/test_hal_tcp_gsp2.py
+++ storm_control/test/test_hal_tcp_gsp2.py
@@ -3,12 +3,12 @@
 from storm_control.test.hal.standardHalTest import halTest
 
 
-def test_hal_gs1():
+def test_hal_gsp2():
 
     halTest(config_xml = "none_tcp_config.xml",
-            class_name = "GetStagePosition1",
+            class_name = "GetStagePosition2",
             test_module = "storm_control.test.hal.tcp_tests")
 
 
 if (__name__ == "__main__"):
-    test_hal_gs1()
+    test_hal_gsp2()
