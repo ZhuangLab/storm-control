2f6187afbcc0236c200b0255541fb6fd68d0fcc7
George Emanuel <emanuega0@gmail.com>
2017-12-01 16:27:33 -0500

    Added parameter to specify whether the daq should be triggered from the falling or rising edge of the camera output

--- storm_control/sc_hardware/nationalInstruments/nidaqModule.py
+++ storm_control/sc_hardware/nationalInstruments/nidaqModule.py
@@ -375,7 +375,14 @@ class NidaqModule(daqModule.DaqModule):
                                                            frequency = frequency, 
                                                            duty_cycle = 0.5)
                     self.ct_task.setCounter(number_samples = self.oversampling)
-                    self.ct_task.setTrigger(trigger_source = self.timing.get("camera_fire_pin"))
+
+                    rising_edge = True
+                    if self.timing.has("rising_edge"):
+                        rising_edge = self.timing.get("rising_edge")
+
+                    self.ct_task.setTrigger(
+                            trigger_source = self.timing.get("camera_fire_pin"),
+                            rising_edge = rising_edge)
                     self.ct_task.startTask()
                 except nicontrol.NIException as exception:
                     print(exception)
