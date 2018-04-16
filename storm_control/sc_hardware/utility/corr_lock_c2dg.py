4f2e5f5e88a2eb335162fc224e8fd00c26ca2029
Hazen Babcock <hbabcock@fas.harvard.edu>
2018-04-16 11:04:20 -0400

    Fix test.

--- storm_control/sc_hardware/utility/corr_lock_c2dg.py
+++ storm_control/sc_hardware/utility/corr_lock_c2dg.py
@@ -82,11 +82,18 @@ if (__name__ == "__main__"):
 
     if True:
 
-        clf = CorrLockFitter(sigma = 1.0, threshold = 0.1)
+        clf = CorrLockFitter(roi_size = 8, sigma = 1.0, threshold = 0.1)
         for i in range(10):
             x = float(i/10.0)
             image1 = dg.drawGaussiansXY((50,200),
                                         numpy.array([25 + x]),
                                         numpy.array([124 + x + 0.2]))
-            print(clf.findFitPeak(image1))
-        
+            [ox, oy, success] = clf.findFitPeak(image1)
+            assert(success)
+            assert(numpy.allclose(numpy.array([ox,oy]),
+                                  numpy.array([25.0 + x, 124 + x + 0.2]),
+                                  atol = 1.0e-3,
+                                  rtol = 1.0e-3))
+            
+                   
+            
