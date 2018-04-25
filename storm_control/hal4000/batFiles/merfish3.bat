a5a3a97cf536ea78b33d2746ad2855cd35c82b06
leonardosepulveda <leonardosepulveda@gmail.com>
2018-04-25 15:35:37 -0400

    added description on how to create a desktop shorcut to Hal

--- storm_control/hal4000/batFiles/merfish3.bat
+++ storm_control/hal4000/batFiles/merfish3.bat
@@ -1,2 +1,4 @@
-call activate halenv
-python -V
+C:\Users\Leonardo\Anaconda3\Scripts\activate.bat  C:\Users\Leonardo\Anaconda3\envs\halenv && cd C:\Software\storm-control\storm_control\hal4000 && python hal4000.py xml\none_config.xml
+
+
+
