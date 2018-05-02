7c2c024ec84012cefa8403eaf17758716d992add
leonardosepulveda <leonardosepulveda@gmail.com>
2018-05-02 13:33:57 -0400

    creating config file for merfish3

--- storm_control/hal4000/batFiles/merfish3.bat
+++ storm_control/hal4000/batFiles/merfish3.bat
@@ -1,3 +1,3 @@
-C:\Users\Leonardo\Anaconda3\Scripts\activate.bat  C:\Users\Leonardo\Anaconda3\envs\halenv && cd C:\Software\storm-control\storm_control\hal4000 && python hal4000.py xml\none_config.xml
+C:\Users\Leonardo\Anaconda3\Scripts\activate.bat  C:\Users\Leonardo\Anaconda3\envs\halenv && cd C:\Software\storm-control\storm_control\hal4000 && python hal4000.py xml\MERFISH3hcam_config.xml
 
 
