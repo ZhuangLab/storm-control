bca34903cc46d24a900ec9f7466ea0359d878c61
Hazen Babcock <hbabcock@mac.com>
2018-05-13 15:41:00 -0400

    Add tests for correct handling of back-to-back parameter requests.

--- storm_control/test/test_hal_tcp_sp.py
+++ storm_control/test/test_hal_tcp_sp.py
@@ -38,3 +38,23 @@ def test_hal_tcp_sp_5():
             class_name = "SetParameters5",
             test_module = "storm_control.test.hal.tcp_tests")
 
+
+def test_hal_tcp_sp_6():
+
+    halTest(config_xml = "none_tcp_config.xml",
+            class_name = "SetParameters6",
+            test_module = "storm_control.test.hal.tcp_tests",
+            show_gui = True)
+
+
+def test_hal_tcp_sp_7():
+
+    halTest(config_xml = "none_tcp_config.xml",
+            class_name = "SetParameters7",
+            test_module = "storm_control.test.hal.tcp_tests",
+            show_gui = True)
+
+    
+if (__name__ == "__main__"):
+    test_hal_tcp_sp_7()
+    
