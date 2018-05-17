0e2bfd79a0f5442156d79e21219032d1b3ade513
Hazen Babcock <hbabcock@mac.com>
2018-05-16 20:34:27 -0400

    Also store time since the log start for each message. Add function for grouping by source. Improve processingTime() and queuedTime() so that they will also work on dictionaries of lists.

--- storm_control/sc_library/log_timing.py
+++ storm_control/sc_library/log_timing.py
@@ -16,16 +16,28 @@ class Message(object):
     """
     Storage for the timing of a single message.
     """
-    def __init__(self, m_type = None, source = None, time = None, **kwds):
+    def __init__(self, m_type = None, source = None, time = None, zero_time = None, **kwds):
         super().__init__(**kwds)
+        self.created_time = None
         self.m_type = m_type
+        self.n_workers = 0
         self.processing_time = None
         self.queued_time = None
-        self.n_workers = 0
         self.source = source
         
         self.temp = self.parseTime(time)
 
+    def created(self, time):
+        t_time = self.parseTime()
+        self.created_time = (self.temp - t_time).total_seconds()
+
+    def getCreatedTime(self):
+        """
+        Returns the time when the message was created relative to first
+        time in the log file in seconds.
+        """
+        return self.created_time
+        
     def getNWorkers(self):
         """
         Return the number of workers (QRunnables) that were employed
@@ -79,37 +91,74 @@ class Message(object):
         self.temp = t_time
 
 
-def groupByMType(messages, ignore_incomplete = True):
+def getIterable(dict_or_list):
+    """
+    Returns an iterable given a dictionary of a list.
+    """
+    if isinstance(dict_or_list, dict):
+        iterable = list(dict_or_list.values())
+    elif isinstance(dict_or_list, list):
+        iterable = dict_or_list
+    else:
+        raise Exception("Unknown type '" + str(type(dict_or_list)) + "'")
+    
+    return iterable
+
+
+def groupByMsgType(messages, ignore_incomplete = True):
     """
     Returns a dictionary keyed by message type, with a list of one or
     more message objects per message type.
     """
-    mtype_groups = {}
-    for msg in messages.values():
+    return groupByX(lambda x : x.getType(),
+                    messages,
+                    ignore_incomplete)
+
+
+def groupBySource(messages, ignore_incomplete = True):
+    """
+    Returns a dictionary keyed by message source, with a list of one or
+    more message objects per message source.
+    """
+    return groupByX(lambda x : x.getSource(),
+                    messages,
+                    ignore_incomplete)
+
+
+def groupByX(grp_fn, messages, ignore_incomplete):
+    """
+    Returns a dictionary keyed by the requested group.
+    """
+    m_grp = {}
+    
+    for msg in getIterable(messages):
 
         # Ignore messages that we don't have all the timing for.
         if msg.isComplete() or not ignore_incomplete:
-            m_type = msg.getType()
-            if m_type in mtype_groups:
-                mtype_groups[m_type].append(msg)
+            m_type = grp_fn(msg)
+            if m_type in m_grp:
+                m_grp[m_type].append(msg)
             else:
-                mtype_groups[m_type] = [msg]
+                m_grp[m_type] = [msg]
                 
-    return mtype_groups
+    return m_grp
         
 
 def logTiming(basename):
     """
     Returns a dictionary of Message objects keyed by their ID number.
     """
+    zero_time = None
     messages = {}
 
     for ext in [".5", ".4", ".3", ".2", ".1", ""]:
 
-        if not os.path.exists(basename + ".out" + ext):
+        fname = basename + ".out" + ext
+        if not os.path.exists(fname):
+            print(fname, "not found.")
             continue
     
-        with open(sys.argv[1] + ".out" + ext) as fp:
+        with open(fname) as fp:
             for line in fp:
 
                 try:
@@ -117,12 +166,16 @@ def logTiming(basename):
                 except ValueError:
                     continue
 
+                if zero_time is None:
+                    zero_time = time
+
                 # Message queued.
                 if (command.startswith("queued,")):
                     [m_id, source, m_type] = command.split(",")[1:]
                     messages[m_id] = Message(m_type = m_type,
                                              source = source,
-                                             time = time)
+                                             time = time,
+                                             zero_time = zero_time)
                               
                 # Message sent.
                 elif (command.startswith("sent,")):
@@ -137,27 +190,35 @@ def logTiming(basename):
                 elif (command.startswith("worker done,")):
                     m_id = command.split(",")[1]
                     messages[m_id].incNWorkers()
-
+    
     return messages
 
 
-def processingTime(list_of_messages):
+def processingTime(messages):
     """
-    Returns the total processing time for a list of messages.
+    Returns the total processing time for a collection of messages.
     """
     accum_time = 0
-    for msg in list_of_messages:
-        accum_time += msg.getProcessingTime()
+    for msg in getIterable(messages):
+        if isinstance(msg, list):
+            for elt in msg:
+                accum_time += elt.getProcessingTime()
+        else:
+            accum_time += msg.getProcessingTime()
     return accum_time
 
 
-def queuedTime(list_of_messages):
+def queuedTime(messages):
     """
-    Returns the total queued time for a list of messages.
+    Returns the total queued time for a a collection of messages.
     """
     accum_time = 0
-    for msg in list_of_messages:
-        accum_time += msg.getQueuedTime()
+    for msg in getIterable(messages):
+        if isinstance(msg, list):
+            for elt in msg:
+                accum_time += elt.getQueuedTime()
+        else:
+            accum_time += msg.getQueuedTime()
     return accum_time
 
 
@@ -170,12 +231,23 @@ if (__name__ == "__main__"):
         exit()
 
     messages = logTiming(sys.argv[1])
-    groups = groupByMType(messages)
-    
+    groups = groupByMsgType(messages)
+
+    print()
+    print("All messages:")
     for key in sorted(groups):
         grp = groups[key]
         print(key + ", {0:0d} counts, {1:.3f} seconds".format(len(grp), processingTime(grp)))
+    print("Total queued time {0:.3f} seconds".format(queuedTime(groups)))
+    print("Total processing time {0:.3f} seconds".format(processingTime(groups)))
 
+    print()
+    print("Film messages:")
+    groups = groupByMsgType(groupBySource(messages)["film"])
+    for key in sorted(groups):
+        grp = groups[key]
+        print(key + ", {0:0d} counts, {1:.3f} seconds".format(len(grp), processingTime(grp)))
+    print("Total processing time {0:.3f} seconds".format(processingTime(groups)))
 
 
 
