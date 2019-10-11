c00a5e4f4f8bd1e8c41104bfa4e49f17c1fcde5b
Hazen Babcock <hbabcock@mac.com>
2019-10-11 12:57:12 -0400

    Add modules for controlling PI stages. Kindly provided by Alistair Boettiger.

---
+++ storm_control/sc_hardware/physikInstrumente/piVoltageZModule.py
@@ -0,0 +1,29 @@
+'''
+PI voltage Z module
+
+Alistair Boettiger, 07/19
+adapted from Ludl (voltage controlled) Z stage functionality, George 02/18
+not currently implemented -- it looks like I get all I need from mclVoltageZModule
+possibly update for aesthetic quality later on.
+'''
+
+from PyQt5 import QtCore
+
+import storm_control.sc_hardware.baseClasses.voltageZModule as voltageZModule
+
+
+class PiVoltageZ(voltageZModule.VoltageZ):
+    """
+    This is a taken from the Mad City Labs stage in analog control mode.
+    """
+    def __init__(self, module_params = None, qt_settings = None, **kwds):
+        super().__init__(module_params, qt_settings, **kwds)
+
+    def handleResponse(self, message, response):
+        if message.isType("get functionality"):
+            self.z_stage_functionality = voltageZModule.VoltageZFunctionality(
+                ao_fn = response.getData()["functionality"],
+                parameters = self.configuration.get("parameters"),
+                microns_to_volts = self.configuration.get("microns_to_volts"),
+                invert_signal = True)
+
