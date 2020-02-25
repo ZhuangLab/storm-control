58af57d198bd318ab745b5e44aa31216fa6286ee
Jeffrey Moffitt <jeffrey.moffitt@childrens.harvard.edu>
2020-02-25 12:55:55 -0500

    Added a multicamera upgrade for Hamamatsu cameras

--- storm_control/sc_hardware/hamamatsu/hamamatsu_camera.py
+++ storm_control/sc_hardware/hamamatsu/hamamatsu_camera.py
@@ -820,8 +820,6 @@ class HamamatsuCameraMR(HamamatsuCamera):
         self.hcam_ptr = False
         self.old_frame_bytes = -1
 
-        self.setPropertyValue("output_trigger_kind[0]", 2)
-
     def getFrames(self):
         """
         Gets all of the available frames.
