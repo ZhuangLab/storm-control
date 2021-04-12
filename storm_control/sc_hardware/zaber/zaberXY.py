b217597d4cc646de3ebd0139a3b98c9572859658
Jeffrey Moffitt <jeffrey.moffitt@childrens.harvard.edu>
2021-04-12 14:50:55 -0400

    Initial commit of zaber stage classes

---
+++ storm_control/sc_hardware/zaber/zaberXY.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+"""
+RS232 interface to a Marzhauser stage.
+
+Hazen 04/17
+"""
+import traceback
+
+import storm_control.sc_hardware.serial.RS232 as RS232
+import storm_control.sc_library.hdebug as hdebug
+
+
+class MarzhauserRS232(RS232.RS232):
+    """
+    Marzhauser RS232 interface class.
+    """
+    def __init__(self, **kwds):
+        """
+        Connect to the Marzhauser stage at the specified port.
+        """
+        self.live = True
+        self.unit_to_um = 1000.0
+        self.um_to_unit = 1.0/self.unit_to_um
+        self.x = 0.0
+        self.y = 0.0
+
+        # RS232 stuff
+        try:
+            super().__init__(**kwds)
+            test = self.commWithResp("?version")
+            if not test:
+                self.live = False
+
+        except (AttributeError, AssertionError):
+            print(traceback.format_exc())
+            self.live = False
+            print("Marzhauser Stage is not connected? Stage is not on?")
+            print("Failed to connect to the Marzhauser stage at port", kwds["port"])
+
+    def goAbsolute(self, x, y):
+        x = x * self.um_to_unit
+        y = y * self.um_to_unit
+        # FIXME: Too many digits here? Use format() instead? int()?"
+        self.writeline(" ".join(["!moa", str(x), str(y)]))
+
+    def goRelative(self, x, y):
+        x = x * self.um_to_unit
+        y = y * self.um_to_unit
+        self.writeline(" ".join(["!mor", str(x), str(y)]))
+
+    def jog(self, x_speed, y_speed):
+        vx = x_speed * self.um_to_unit
+        vy = y_speed * self.um_to_unit
+        self.writeline(" ".join(["!speed ", str(vx), str(vy)]))
+        
+    def joystickOnOff(self, on):
+        if on:
+            self.writeline("!joy 2")
+        else:
+            self.writeline("!joy 0")
+
+    def position(self):
+        self.writeline("?pos")
+
+    def serialNumber(self):
+        """
+        Return the stages serial number.
+        """
+        return self.writeline("?readsn")
+
+    def setVelocity(self, x_vel, y_vel):
+        self.writeline(" ".join(["!vel",str(x_vel),str(y_vel)]))
+    
+    def setAcceleration(self, x_accel, y_accel):
+        self.writeline(" ".join(["!accel",str(x_accel),str(y_accel)]))
+
+    def zero(self):
+        self.writeline("!pos 0 0")
+
+
+#
+# Testing
+#
+if (__name__ == "__main__"):
+    import time
+
+    stage = MarzhauserRS232(port = "COM1", baudrate = 57600)
+    
+    def comm(cmd, timeout):
+        cmd()
+        time.sleep(timeout)
+        return stage.readline()
+    
+    if stage.getStatus():
+
+        # Test communication.
+        if False:
+            print("SN:", comm(stage.serialNumber, 0.1))
+            print("zero:", comm(stage.zero, 0.1))
+            print("position:", comm(stage.position, 0.1))
+            print("goAbsolute:", comm(lambda: stage.goAbsolute(100,100), 0.5))
+            print("position:", comm(stage.position, 0.1))
+            print("goRelative:", len(comm(lambda: stage.goRelative(100,100), 0.5)))
+            print("position:", comm(stage.position, 0.1))
+
+        # Test whether we can jam up stage communication.
+        if True:
+            reps = 20
+            for i in range(reps):
+                print(i)
+                stage.position()
+                stage.goAbsolute(i*10,0)
+                stage.position()
+                time.sleep(0.1)
+
+            for i in range(3*reps + 4):
+                responses = stage.readline()
+                for resp in responses.split("\r"):
+                    print(i, resp, len(resp))
+            
+        stage.shutDown()
+
+
+#
+# The MIT License
+#
+# Copyright (c) 2017 Zhuang Lab, Harvard University
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
---
+++ storm_control/sc_hardware/zaber/zaberXYModule.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+"""
+HAL module for controlling a Marzhauser stage.
+
+Hazen 04/17
+"""
+
+from PyQt5 import QtCore
+
+import storm_control.hal4000.halLib.halMessage as halMessage
+
+import storm_control.sc_hardware.baseClasses.stageModule as stageModule
+import storm_control.sc_hardware.marzhauser.marzhauser as marzhauser
+
+
+class MarzhauserStageFunctionality(stageModule.StageFunctionality):
+    """
+    These stages are nice because they respond quickly to commands
+    and they also provide feedback about whether or not they are
+    moving.
+    """
+    def __init__(self, update_interval = None, **kwds):
+        super().__init__(**kwds)
+        self.querying = False
+
+        # Each time this timer fires we'll 'query' the stage for it's
+        # current position.
+        self.updateTimer = QtCore.QTimer()
+        self.updateTimer.setInterval(update_interval)
+        self.updateTimer.timeout.connect(self.handleUpdateTimer)
+        self.updateTimer.start()
+
+        # Connect to our own stagePosition signal in order to store
+        # the current position.
+        self.stagePosition.connect(self.handleStagePosition)
+        
+        # This thread will poll the serial port for responses from
+        # the stage to the commands we're sending.
+        self.polling_thread = MarzhauserPollingThread(device_mutex = self.device_mutex,
+                                                      is_moving_signal = self.isMoving,
+                                                      sleep_time = 100,
+                                                      stage = self.stage,
+                                                      stage_position_signal = self.stagePosition)
+        self.polling_thread.startPolling()
+
+    def handleStagePosition(self, pos_dict):
+        self.pos_dict = pos_dict
+        self.querying = False
+
+    def handleUpdateTimer(self):
+        """
+        Query the stage for its current position.
+        """
+        #
+        # The purpose of the self.querying flag is to prevent build up of
+        # position update requests. If there is already one in process there
+        # is no point in starting another one.
+        #
+        if not self.querying:
+            self.querying = True
+            self.mustRun(task = self.stage.position)
+
+    def wait(self):
+        self.updateTimer.stop()
+        self.polling_thread.stopPolling()
+        super().wait()
+
+
+class MarzhauserPollingThread(QtCore.QThread):
+    """
+    Handles polling the Marzhauser stage for responses to 
+    serial commands.
+    """
+    def __init__(self,
+                 device_mutex = None,
+                 is_moving_signal = None,
+                 sleep_time = None,
+                 stage = None,
+                 stage_position_signal = None,
+                 **kwds):
+        super().__init__(**kwds)
+        self.device_mutex = device_mutex
+        self.is_moving_signal = is_moving_signal
+        self.sleep_time = sleep_time         
+        self.stage = stage
+        self.stage_position_signal = stage_position_signal
+
+    def run(self):
+        self.running = True
+        while(self.running):
+            self.device_mutex.lock()
+            responses = self.stage.readline()
+            self.device_mutex.unlock()
+            
+            # Parse response. The expectation is that it is one of two things:
+            #
+            # (1) A status string like "#@--" that indicates that the stage
+            #     is or is not moving (statusaxis).
+            #
+            # (2) The current position "X.XX Y.YY ..".
+            #
+
+            for resp in responses.split("\r"):
+
+                # The response was no response.
+                if (len(resp) == 0):
+                    continue
+                
+                # Check for 'statusaxis' response form.
+                elif (len(resp) == 5):
+                    if (resp[:2] == "@@"):
+                        self.is_moving_signal.emit(False)
+                    else:
+                        self.is_moving_signal.emit(True)
+
+                # Otherwise try and parse as a position.
+                else:
+                    resp = resp.split(" ")
+                    if (len(resp) >= 2):
+                        are_floats = True
+                        try:
+                            [sx, sy] = map(float, resp[:2])
+                        except ValueError:
+                            are_floats = False
+                        if are_floats:
+                            self.stage_position_signal.emit({"x" : sx * self.stage.unit_to_um,
+                                                             "y" : sy * self.stage.unit_to_um})
+
+            # Sleep for ~ x milliseconds.
+            self.msleep(self.sleep_time)
+
+    def startPolling(self):
+        self.start(QtCore.QThread.NormalPriority)
+
+    def stopPolling(self):
+        self.running = False
+        self.wait()
+        
+
+class MarzhauserStage(stageModule.StageModule):
+
+    def __init__(self, module_params = None, qt_settings = None, **kwds):
+        super().__init__(**kwds)
+
+        configuration = module_params.get("configuration")
+        self.stage = marzhauser.MarzhauserRS232(baudrate = configuration.get("baudrate"),
+                                                port = configuration.get("port"))
+        if self.stage.getStatus():
+
+            # Set (maximum) stage velocity.
+            velocity = configuration.get("velocity")
+            self.stage.setVelocity(velocity, velocity)
+            self.stage_functionality = MarzhauserStageFunctionality(device_mutex = QtCore.QMutex(),
+                                                                    stage = self.stage,
+                                                                    update_interval = 500)
+
+        else:
+            self.stage = None
