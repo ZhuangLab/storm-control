b395ca29ad2f2daa520e21a1000a5810838485f1
aaron <7842040+aaronhalpern@users.noreply.github.com>
2022-03-16 14:35:13 -0400

    dave bat update

--- storm_control/dave/dave.bat
+++ storm_control/dave/dave.bat
@@ -1,2 +1,6 @@
 rem Activate virtual environment and start Dave.
-c:\users\hazen\pyenv\hal2\scripts\activate && python dave.py
+rem c:\users\hazen\pyenv\hal2\scripts\activate && python dave.py
+call activate hal
+cd C:\Users\aaron\storm-control\storm_control\dave
+python dave.py
+pause
